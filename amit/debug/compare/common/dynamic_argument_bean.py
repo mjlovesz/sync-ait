21db3069f8f623b9ec744977ba52fa6261433423
wujianwei.wu <wujianwei.wu@huawei.com>
2023-04-13 11:47:44 +0800

    一键式精度比对推理接口从msame替换成ais_bench

--- amit/debug/compare/common/dynamic_argument_bean.py
+++ amit/debug/compare/common/dynamic_argument_bean.py
@@ -2,14 +2,14 @@ from enum import Enum
 
 
 class Arg(object):
-    def __init__(self, msame_arg, atc_arg, msquickcmp_arg):
+    def __init__(self, benchkmark_arg, atc_arg, msquickcmp_arg):
         self.atc_arg = atc_arg
-        self.msame_arg = msame_arg
+        self.benchkmark_arg = benchkmark_arg
         self.msquickcmp_arg = msquickcmp_arg
 
 
 class DynamicArgumentEnum(Enum):
-    # enum struct Arg(msame_arg, atc_arg, msquickcmp_arg)
+    # enum struct Arg(benchkmark_arg, atc_arg, msquickcmp_arg)
     DYM_BATCH = Arg("--dymBatch", "--dynamic_batch_size", None)
     DYM_SHAPE = Arg("--dymShape", "--input_shape_range", "input_shape")
     DYM_DIMS = Arg("--dymDims", "--dynamic_dims", "input_shape")
