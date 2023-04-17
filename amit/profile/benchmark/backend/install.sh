f0becc9bb34e54db28b124a311484efc319070f4
wujianwei.wu <wujianwei.wu@huawei.com>
2023-04-17 11:07:34 +0800

    检视意见修改

--- amit/profile/benchmark/backend/install.sh
+++ amit/profile/benchmark/backend/install.sh
@@ -2,7 +2,6 @@
 
 declare -i ret_ok=0
 declare -i ret_run_failed=1
-PYTHON_COMMAND="python3"
 
 check_python_package_is_install()
 {
@@ -25,6 +24,23 @@ check_env_valid()
 
 main()
 {
+      while [ -n "$1" ]
+do
+  case "$1" in
+    -p|--python_command)
+        PYTHON_COMMAND=$2
+        shift
+        ;;
+    *)
+        echo "$1 is not an option, please use --help"
+        exit 1
+        ;;
+  esac
+  shift
+done
+
+    [ "$PYTHON_COMMAND" != "" ] || { PYTHON_COMMAND="python3.7";echo "set default pythoncmd:$PYTHON_COMMAND"; }
+
     check_env_valid
     res=`echo $?`
     if [ $res =  $ret_run_failed ]; then
