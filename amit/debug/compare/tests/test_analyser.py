ee432e85630264ec8aab22ba99be9afa9285d709
王国伟 <leondgarse@163.com>
2023-04-19 02:54:02 +0000

    update amit/debug/compare/tests/test_analyser.py.
    Signed-off-by: 王国伟 <leondgarse@163.com>

--- amit/debug/compare/tests/test_analyser.py
+++ amit/debug/compare/tests/test_analyser.py
@@ -1,6 +1,19 @@
-# Copyright Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+# Copyright 2023 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import os
+import stat
 import pytest
 
 from compare import analyser
@@ -28,9 +41,12 @@ def fake_csv():
         "953,Add,Add_1179,float16,Add_1179,0.905575,0.429081,inf,4.061347,5254036,2.989594",
         "955,NaN,Node_Output,NaN,output_0.npy,0.704,0.761698,inf,3419.279248,63.709677,1.655235",
     ]
-    with open(test_csv_file_name, "w") as ff:
-        ff.write(",".join(columns) + "\n")
-        ff.write("\n".join(data))
+
+    flags = os.O_WRONLY | os.O_CREAT | os.O_EXCL  # 注意根据具体业务的需要设置文件读写方式
+    modes = stat.S_IWUSR | stat.S_IRUSR  # 注意根据具体业务的需要设置文件权限
+    with os.fdopen(os.open(test_csv_file_name, flags, modes), 'w') as fout:
+        fout.write(",".join(columns) + "\n")
+        fout.write("\n".join(data))
 
     yield test_csv_file_name
 
