14272bcd5f0a2329649e848578168115887bea6e
huanghao <huanghao122@huawei.com>
2023-04-21 14:28:10 +0800

    benchmark entry_point

--- amit/profile/profile.py
+++ amit/profile/profile.py
@@ -1,8 +1,9 @@
 import click
+import pkg_resources
+from main_cli import benchmark_cli_enter
 
-from profile.benchmark.main_cli import benchmark_cli_enter
+profile_sub_task = {}
+for entry_point in pkg_resources.iter_entry_points('debug_sub_task'):
+    debug_sub_task[entry_point.name] = entry_point.load()
 
-profile_cli_group = click.Group(name="profile", commands=[benchmark_cli_enter])
-
-if __name__ == "__main__":
-    profile_cli_group()
+profile_cli_group = click.Group(name="profile", commands=profile_sub_task)
