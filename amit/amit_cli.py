603388d1fb7474847e6cb32cb707800c5b8d2519
Jianwei.Wu <wujianwei.wu@huawei.com>
2023-04-23 11:48:29 +0800

    amit setup

--- amit/amit_cli.py
+++
@@ -1,9 +0,0 @@
-import click
-
-from debug.debug import debug_cli_group
-from profile.profile import profile_cli_group
-
-cli = click.Group(commands=[debug_cli_group, profile_cli_group])
-
-if __name__ == "__main__":
-    cli()
