cc2b592e0e22ed2ed0142d6e819589f69baa1e5b
huanghao <huanghao122@huawei.com>
2023-04-23 16:25:49 +0800

    profile入口修改

---
+++ amit/profile/profile.py
@@ -0,0 +1,8 @@
+import click
+import pkg_resources
+
+profile_sub_task = {}
+for entry_point in pkg_resources.iter_entry_points('profile_sub_task'):
+    profile_sub_task[entry_point.name] = entry_point.load()
+
+profile_cli_group = click.Group(name="profile", commands=profile_sub_task)
