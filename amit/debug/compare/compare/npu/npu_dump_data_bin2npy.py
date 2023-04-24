3a8659fd29b989c020a204631781a1e59bdb713b
TAJh <taojiaheng1@huawei.com>
2023-04-24 03:58:25 +0000

    规范格式，增加空格

--- amit/debug/compare/compare/npu/npu_dump_data_bin2npy.py
+++ amit/debug/compare/compare/npu/npu_dump_data_bin2npy.py
@@ -16,7 +16,7 @@ import sys
 
 from compare.common import utils
 
-MSACCUCMP_FILE_PATH =  "toolkit/tools/operator_cmp/compare/msaccucmp.py"
+MSACCUCMP_FILE_PATH = "toolkit/tools/operator_cmp/compare/msaccucmp.py"
 
 
 def data_convert(npu_dump_data_path, npu_net_output_data_path, arguments):
@@ -30,14 +30,14 @@ def data_convert(npu_dump_data_path, npu_net_output_data_path, arguments):
         common_path = os.path.commonprefix([npu_dump_data_path, npu_net_output_data_path])
         npu_dump_data_path_diff = os.path.relpath(npu_dump_data_path, common_path)
         time_stamp_file_path = npu_dump_data_path_diff.split(os.path.sep)[1]
-        convert_dir_path = npu_dump_data_path.replace(time_stamp_file_path, time_stamp_file_path+'_bin2npy')
+        convert_dir_path = npu_dump_data_path.replace(time_stamp_file_path, time_stamp_file_path + '_bin2npy')
         convert_dir_path = os.path.normpath(convert_dir_path)
         convert_data_path = _check_data_convert_file(convert_dir_path)
         msaccucmp_command_file_path = os.path.join(arguments.cann_path, MSACCUCMP_FILE_PATH)
         python_version = sys.executable.split('/')[-1]
-        bin2npy_cmd = [python_version, msaccucmp_command_file_path,"convert","-d",npu_dump_data_path,"-out",convert_data_path]
+        bin2npy_cmd = [python_version, msaccucmp_command_file_path, "convert", "-d", npu_dump_data_path, "-out", convert_data_path]
         utils.execute_command(bin2npy_cmd)
-        utils.print_info_log("msaccucmp command line: %s "%" ".join(bin2npy_cmd))
+        utils.print_info_log("msaccucmp command line: %s " % " ".join(bin2npy_cmd))
 
 
 def _check_data_convert_file(convert_dir_path):
