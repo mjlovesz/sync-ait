3c2b097a14b4885c14867eb11f0b016396340872
TAJh <taojiaheng1@huawei.com>
2023-04-24 11:46:31 +0000

    修正import顺序
    Signed-off-by: TAJh <taojiaheng1@huawei.com>

--- amit/debug/compare/compare/common/tf_common.py
+++ amit/debug/compare/compare/common/tf_common.py
@@ -12,9 +12,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
+import subprocess
+
 import numpy as np
 import tensorflow as tf
-import subprocess
 
 from compare.common import utils
 from compare.common.utils import AccuracyCompareException
