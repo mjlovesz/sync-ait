303365f4daab46e24d78136c1b0ae10873a2378d
yanhe13 <yanhe13@huawei.com>
2023-04-25 15:03:35 +0800

    codecheck fix v0.12

--- amit/profile/benchmark/ais_bench/infer/interface.py
+++ amit/profile/benchmark/ais_bench/infer/interface.py
@@ -1,3 +1,18 @@
+"""
+Copyright(C) 2021. Huawei Technologies Co.,Ltd. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 import logging
 import time
@@ -305,8 +320,9 @@ class InferSession:
             tmp_padding_params.append(
                 0 if option_list.count('padding_size_left') == 0 else cfg.getint('aipp_op', 'padding_size_left')
             )
-            tmp_padding_params.append(0 if option_list.count('padding_size_right') == 0 \
-                else cfg.getint('aipp_op', 'padding_size_right'))
+            tmp_padding_params.append(
+                0 if option_list.count('padding_size_right') == 0 else cfg.getint('aipp_op', 'padding_size_right')
+            )
 
             range_ok = True
             for i in range (1, 5):
