965c302f1e313dba57e43f465b05da871b9d8238
qianzhengxin <qianzhengxin@huawei.com>
2023-04-26 15:42:57 +0800

    no_msprof + cleanCode

--- amit/profile/benchmark/ais_bench/infer/__main__.py
+++ amit/profile/benchmark/ais_bench/infer/__main__.py
@@ -453,7 +453,7 @@ if __name__ == "__main__":
 
     if type(args.device) == list:
         # args has multiple device, run single process for each device
-        multidevice_ret = multidevice_run(args)
-        exit(multidevice_ret)
+        multi_device_ret = multidevice_run(args)
+        exit(multi_device_ret)
 
     main(args)
