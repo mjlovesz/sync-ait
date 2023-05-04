56b31bbecc1106a6631c895aa1cda461475f0008
wujianwei.wu <wujianwei.wu@huawei.com>
2023-05-04 16:43:14 +0800

    batch_size

--- ait/components/profile/benchmark/ais_bench/infer/main_cli.py
+++ ait/components/profile/benchmark/ais_bench/infer/main_cli.py
@@ -77,14 +77,14 @@ from ais_bench.infer.options import (
 def benchmark_cli(om_model, input_path, output, 
                   output_dirname, outfmt, loop, debug, device,
                   dym_batch, dym_hw, dym_dims, dym_shape, output_size, auto_set_dymshape_mode,
-                  auto_set_dymdims_mode, batchsize, pure_data_type, profiler, dump,
+                  auto_set_dymdims_mode, batch_size, pure_data_type, profiler, dump,
                   acl_json_path, output_batchsize_axis, run_mode, display_all_summary,
                   warmup_count, dym_shape_range, aipp_config):
 
     args = BenchMarkArgsAdapter(om_model.as_posix(), input_path, output.as_posix() if output else None, 
                                 output_dirname, outfmt, loop, debug, device,
                                 dym_batch, dym_hw, dym_dims, dym_shape, output_size, auto_set_dymshape_mode,
-                                auto_set_dymdims_mode, batchsize, pure_data_type, profiler, dump,
+                                auto_set_dymdims_mode, batch_size, pure_data_type, profiler, dump,
                                 acl_json_path, output_batchsize_axis, run_mode, display_all_summary,
                                 warmup_count, dym_shape_range, aipp_config)
     benchmark_process(args)
