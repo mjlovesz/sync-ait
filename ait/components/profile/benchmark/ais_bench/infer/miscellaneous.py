97c4bcb5b2eadfb06fb0d834d8fb1b0510a7e107
wujianwei.wu <wujianwei.wu@huawei.com>
2023-05-08 15:18:20 +0800

    dymshape_range_run

--- ait/components/profile/benchmark/ais_bench/infer/miscellaneous.py
+++ ait/components/profile/benchmark/ais_bench/infer/miscellaneous.py
@@ -139,8 +139,12 @@ def dymshape_range_run(args):
     results = []
     log_path = "./dym.log" if args.output is None else args.output + "/dym.log"
     for dymshape in dymshape_list:
-        cmd = "rm -rf {};{} {} {}".format(log_path, sys.executable, ' '.join(sys.argv),
-            "--dymShape={}  | tee {}".format(dymshape,  log_path))
+        if ('ait profile benchmark' in sys.executable):
+            cmd = "rm -rf {};{} {} {}".format(log_path, sys.executable, ' '.join(sys.argv),
+                "--dym-shape={}  | tee {}".format(dymshape,  log_path))
+        else:
+            cmd = "rm -rf {};{} {} {}".format(log_path, sys.executable, ' '.join(sys.argv),
+                "--dymShape={}  | tee {}".format(dymshape,  log_path))
         result = { "dymshape" : dymshape, "cmd": cmd, "result": "Failed", "throughput" : 0 }
         logger.debug("cmd:{}".format(cmd))
         os.system(cmd)
