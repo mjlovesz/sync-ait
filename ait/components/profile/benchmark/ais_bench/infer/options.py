c814452bdee13d53367d8dc4c12a1021cdec3716
wujianwei.wu <wujianwei.wu@huawei.com>
2023-05-08 14:50:32 +0800

    warmup_count

--- ait/components/profile/benchmark/ais_bench/infer/options.py
+++ ait/components/profile/benchmark/ais_bench/infer/options.py
@@ -318,7 +318,7 @@ opt_display_all_summary = click.option(
 
 opt_warmup_count = click.option(
     '-wcount',
-    '--warmup-count',
+    '--warmup_count',
     'warmup_count',
     default=1,
     type=int,
