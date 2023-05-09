83e897f5af77baf2460fd4e698361c8ef0b94e41
qianzhengxin <qianzhengxin@huawei.com>
2023-05-09 14:12:34 +0800

    dump acl_json check v4

--- ait/components/profile/benchmark/ais_bench/infer/miscellaneous.py
+++ ait/components/profile/benchmark/ais_bench/infer/miscellaneous.py
@@ -33,6 +33,8 @@ def check_valid_acljson(acl_json_path, model):
         dump_list_val = acl_json_dict["dump"].get("dump_list")
         if dump_list_val is not None:
             if dump_list_val == [] or dump_list_val[0].get("model_name") != model_name_correct:
+                print("correct is {}".format(model_name_correct))
+                print("incorrect is {}"/format(dump_list_val[0].get("model_name")))
             # model_name_val = acl_json_dict["dump"]["dump_list"][0].get("model_name")
             # if model_name_val != model_name_correct:
                 logger.warning("dump failed, 'model_name' is not set or set incorrectly")
