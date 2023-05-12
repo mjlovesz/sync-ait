b6e2cfd00f83c7bcd0c050343f41cf21adcaf08f
huanghao <huanghao122@huawei.com>
2023-05-12 16:24:10 +0800

    修改变量名

--- ait/install.sh
+++ ait/install.sh
@@ -4,18 +4,18 @@ CURRENT_DIR=$(dirname $(readlink -f $0))
 arg_force_reinstall=
 arg_help=0
 
-while [["$#" -gt 0]]; do case $1 in
+while [[ "$#" -gt 0 ]]; do case $1 in
   --force-reinstall) arg_force_reinstall=--force-reinstall;;
   -h|--help) arg_help=1;;
   *) echo "Unknown parameter: $1";
 esac; shift; done
 
-pip install CURRENT_DIR arg_force_reinstall
+pip install $CURRENT_DIR arg_force_reinstall
 
-pip install CURRENT_DIR/components/debug/compare arg_force_reinstall
+pip install ${CURRENT_DIR}/components/debug/compare arg_force_reinstall
 
-pip install CURRENT_DIR/components/debug arg_force_reinstall
+pip install ${CURRENT_DIR}/components/debug arg_force_reinstall
 
-pip install CURRENT_DIR/components/profile/benchmark/backend arg_force_reinstall
+pip install ${CURRENT_DIR}/components/profile/benchmark/backend arg_force_reinstall
 
-pip install CURRENT_DIR/components/profile/benchmark arg_force_reinstall
+pip install ${CURRENT_DIR}/components/profile/benchmark arg_force_reinstall
