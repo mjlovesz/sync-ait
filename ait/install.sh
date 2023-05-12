33429b6ed71800ff1956839e4a8a07db0d843b62
huanghao <huanghao122@huawei.com>
2023-05-12 16:32:58 +0800

    添加help说明

--- ait/install.sh
+++ ait/install.sh
@@ -7,8 +7,13 @@ arg_help=0
 while [[ "$#" -gt 0 ]]; do case $1 in
   --force-reinstall) arg_force_reinstall=--force-reinstall;;
   -h|--help) arg_help=1;;
-  *) echo "Unknown parameter: $1";
+  *) echo "Unknown parameter: $1";exit 1;
 esac; shift; done
+
+if [ "$arg_help" -eq "1" ]; then
+  echo "Usage: $0 [options]"
+  echo " --help or -h      : Print help menu"
+  echo " --force-reinstall : reinstall"
 
 pip install ${CURRENT_DIR} ${arg_force_reinstall}
 
@@ -16,6 +21,6 @@ pip install ${CURRENT_DIR}/components/debug/compare ${arg_force_reinstall}
 
 pip install ${CURRENT_DIR}/components/debug/surgeon ${arg_force_reinstall}
 
-pip install ${CURRENT_DIR}/components/profile/benchmark/backend ${arg_force_reinstall}
+pip install -v ${CURRENT_DIR}/components/profile/benchmark/backend ${arg_force_reinstall}
 
 pip install ${CURRENT_DIR}/components/profile/benchmark ${arg_force_reinstall}
