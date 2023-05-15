e200e3c570f0305fa9e90dcb171d417e24bc0d62
huanghao <huanghao122@huawei.com>
2023-05-15 14:56:28 +0800

    修改检视意见

--- ait/install.sh
+++ ait/install.sh
@@ -6,6 +6,7 @@ arg_help=0
 
 while [[ "$#" -gt 0 ]]; do case $1 in
   --force-reinstall) arg_force_reinstall=--force-reinstall;;
+  -f) arg_force_reinstall=--force-reinstall;;
   -h|--help) arg_help=1;;
   *) echo "Unknown parameter: $1";exit 1;
 esac; shift; done
@@ -18,7 +19,13 @@ if [ "$arg_help" -eq "1" ]; then
 fi
 
 if [ ! -z ${arg_force_reinstall} ]; then
-  pip uninstall ait compare aclruntime ais_bench auto-optimizer
+  python3 -m pip uninstall
+  ${CURRENT_DIR}/ait
+  ${CURRENT_DIR}/components/debug/compare
+  ${CURRENT_DIR}/components/debug/surgeon
+  ${CURRENT_DIR}/components/profile/benchmark/backend
+  ${CURRENT_DIR}/components/profile/benchmark
+  --force-reinstall
 fi
 
 pip install ${CURRENT_DIR}
