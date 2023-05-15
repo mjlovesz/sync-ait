c2f3fd26ec93c7d18db908db1933095e987f22da
TAJh <taojiaheng1@huawei.com>
2023-05-15 12:18:19 +0000

    clean code清理
    Signed-off-by: TAJh <taojiaheng1@huawei.com>

--- ait/components/profile/benchmark/test/interface_sample.py
+++ ait/components/profile/benchmark/test/interface_sample.py
@@ -32,9 +32,9 @@ def infer_simple():
 
     # in is numpy list and ouput is numpy list
     outputs = session.infer([ndata])
-    logging.info("outputs:{} type:{}".format(outputs, type(outputs)))
+    logging.info("outputs:{outputs} type:{type(outputs)}")
 
-    logging.info("static infer avg:{} ms".format(np.mean(session.sumary().exec_time_list)))
+    logging.info("static infer avg:{np.mean(session.sumary().exec_time_list)} ms")
 
 
 def infer_torch_tensor():
@@ -45,7 +45,7 @@ def infer_torch_tensor():
     torchtensor = torch.zeros([1,3,256,256], out = None, dtype=torch.uint8)
     # in is torch tensor and ouput is numpy list
     outputs = session.infer([torchtensor])
-    logging.info("in torch tensor outputs[0].shape:{} type:{}".format(outputs[0].shape, type(outputs)))
+    logging.info("in torch tensor outputs[0].shape:{outputs[0].shape} type:{type(outputs)}")
 
     # create discontinuous torch tensor
     torchtensor = torch.zeros([1,256,3,256], out = None, dtype = torch.uint8)
@@ -53,9 +53,9 @@ def infer_torch_tensor():
 
     # in is discontinuous tensor list and ouput is numpy list
     outputs = session.infer([torchtensor_discontinue])
-    logging.info("in discontinuous torch tensor outputs[0].shape:{} type:{}".format(outputs[0].shape, type(outputs)))
+    logging.info("in discontinuous torch tensor outputs[0].shape:{outputs[0].shape} type:{type(outputs)}")
 
-    logging.info("static infer avg:{} ms".format(np.mean(session.sumary().exec_time_list)))
+    logging.info("static infer avg:{np.mean(session.sumary().exec_time_list)} ms")
 
 
 def infer_dymshape():
@@ -68,13 +68,13 @@ def infer_dymshape():
     # input args custom_sizes is int
     output_size = 100000
     outputs = session.infer([ndata], mode, custom_sizes=output_size)
-    logging.info("inputs: custom_sizes: {} outputs:{} type:{}".format(output_size, outputs, type(outputs)))
+    logging.info("inputs: custom_sizes: {output_size} outputs:{outputs} type:{type(outputs)}")
 
     # input args custom_sizes is list
     output_sizes = [100000]
     outputs = session.infer([ndata], mode, custom_sizes=output_sizes)
-    logging.info("inputs: custom_sizes: {} outputs:{} type:{}".format(output_sizes, outputs, type(outputs)))
-    logging.info("dymshape infer avg:{} ms".format(np.mean(session.sumary().exec_time_list)))
+    logging.info("inputs: custom_sizes: {output_sizes} outputs:{outputs} type:{type(outputs)}")
+    logging.info("dymshape infer avg:{np.mean(session.sumary().exec_time_list)} ms")
 
 
 def infer_dymdims():
@@ -85,9 +85,9 @@ def infer_dymdims():
 
     mode = "dymdims"
     outputs = session.infer([ndata], mode)
-    logging.info("outputs:{} type:{}".format(outputs, type(outputs)))
+    logging.info("outputs:{outputs} type:{type(outputs)}")
 
-    logging.info("dymdims infer avg:{} ms".format(np.mean(session.sumary().exec_time_list)))
+    logging.info("dymdims infer avg:{np.mean(session.sumary().exec_time_list))} ms")
 
 
 # 获取模型信息
@@ -101,13 +101,11 @@ def get_model_info():
     # 方法3 也可以直接通过get接口去获取
     intensors_desc = session.get_inputs()
     for i, info in enumerate(intensors_desc):
-        logging.info("input info i:{} shape:{} type:{} val:{} realsize:{} size:{}".format(
-            i, info.shape, info.datatype, int(info.datatype), info.realsize, info.size))
+        logging.info("input info i:{i} shape:{info.shape} type:{info.datatype} val:{int(info.datatype)} realsize:{info.realsize} size:{info.size}")
 
     intensors_desc = session.get_outputs()
     for i, info in enumerate(intensors_desc):
-        logging.info("outputs info i:{} shape:{} type:{} val:{} realsize:{} size:{}".format(
-            i, info.shape, info.datatype, int(info.datatype), info.realsize, info.size))
+        logging.info("outputs info i:{i} shape:{info.shape} type:{info.datatype} val:{int(info.datatype)} realsize:{info.realsize} size:{info.size}")
 
 infer_simple()
 
