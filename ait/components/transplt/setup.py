ec886e45a3e3d99f9dc6d05bc24c91ecfe4f5586
jinfaz <zhujinfa@huawei.com>
2023-05-15 14:44:04 +0800

    change name to transplt_analyze

--- ait/components/transplt/setup.py
+++ ait/components/transplt/setup.py
@@ -21,7 +21,7 @@ with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='app_analyze',
+    name='transplt',
     version='0.1.0',
     description='app analysis for cpu and gpu projects',
     long_description=long_description,
