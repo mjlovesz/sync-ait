f66ef68edfd510974f27a1302bfcc6c3e4de1400
JacobHuang <huangjianbin1@huawei.com>
2023-05-17 01:47:18 +0800

    ait add msprof

---
+++ ait/components/profile/msprof/ait_prof/msprof_process.py
@@ -0,0 +1,46 @@
+import logging
+import math
+import os
+import sys
+import time
+import shutil
+import copy
+
+from .utils import logger
+from .args_adapter import MsProfArgsAdapter
+
+def msprof_run_profiling(args, msprof_bin):
+    cmd = sys.executable + " " + ' '.join(sys.argv) + " --profiler=0 --warmup-count=0"
+    msprof_cmd="{} --output={}/profiler --application=\"{}\" --model-execution={}" \
+               " --sys-hardware-mem={} --sys-cpu-profiling={}" \
+               " --sys-profiling={} --sys-pid-profiling={} --dvpp-profiling={} " \
+               "--runtime-api={} --task-time={} --aicpu={}".format(msprof_bin, args.output, args.application,
+                                                                   args.model_execution, args.sys_hardware_mem,
+                                                                   args.sys_cpu_profiling, args.sys_profiling,
+                                                                   args.sys_pid_profiling, args.dvpp_profiling,
+                                                                   args.runtime_api, args.task_time, args.aicpu)
+    logger.info("msprof cmd:{} begin run".format(msprof_cmd))
+    ret = os.system(msprof_cmd)
+    logger.info("msprof cmd:{} end run ret:{}".format(msprof_cmd, ret))
+
+
+def args_rules(args):
+    if args.output is None and args.output_dirname is not None:
+        logger.error("parameter --output_dirname cann't be used alone. Please use it together with the parameter --output!\n")
+        raise RuntimeError('error bad parameters --output_dirname')
+    return args
+
+
+def msprof_process(args:MsProfArgsAdapter):
+    #args = args_rules(args)
+
+    #if args.profiler is True:
+    # try use msprof to run
+    msprof_bin = shutil.which('msprof')
+    if msprof_bin is None or os.getenv('GE_PROFILIGN_TO_STD_OUT') == '1':
+        logger.info("find no msprof continue use acl.json mode")
+    else:
+        msprof_run_profiling(args, msprof_bin)
+        return 0
+
+    return 0
