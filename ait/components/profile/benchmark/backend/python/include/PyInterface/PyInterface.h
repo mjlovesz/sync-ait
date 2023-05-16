e8e60a1b64e62339bbfeff1e2ccba6aaddbeb47e
TAJh <taojiaheng1@huawei.com>
2023-05-16 02:52:08 +0000

    版权头
    Signed-off-by: TAJh <taojiaheng1@huawei.com>

--- ait/components/profile/benchmark/backend/python/include/PyInterface/PyInterface.h
+++ ait/components/profile/benchmark/backend/python/include/PyInterface/PyInterface.h
@@ -2,7 +2,7 @@
 #define PY_INTER_FACE
 
 /*
- * Copyright (c) 2023. Huawei Technologies Co.,Ltd. All rights reserved.
+ * Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
