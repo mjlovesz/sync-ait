e18e051c2a982f0cbba7cecbf1dc2e079518598c
SsTtOoNnEe <1602965550@qq.com>
2023-05-18 11:29:55 +0800

    resolve conflicts

--- ait/components/debug/surgeon/auto_optimizer/graph_refactor/interface/base_graph.py
+++ ait/components/debug/surgeon/auto_optimizer/graph_refactor/interface/base_graph.py
@@ -169,7 +169,7 @@ class BaseGraph(ABC):
         raise NotImplementedError()
 
     @abstractmethod
-    def infershape(self) -> None:
+    def infer_shape(self) -> None:
         raise NotImplementedError()
 
     def update_map(self) -> None:
