f7eb91198a2354a74ca250c3e7224c2c43adb036
JacobHuang <huangjianbin1@huawei.com>
2023-05-18 12:08:45 +0000

    update ait/components/profile/msprof/ait_prof/__main__.py.
    Signed-off-by: JacobHuang <huangjianbin1@huawei.com>

--- ait/components/profile/msprof/ait_prof/__main__.py
+++ ait/components/profile/msprof/ait_prof/__main__.py
@@ -17,17 +17,6 @@ from msprof.msprof_process import msprof_process
 from msprof.args_adapter import MsProfArgsAdapter
 
 
-def str2bool(v):
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ('yes', 'true', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'false', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected true, 1, false, 0 with case insensitive.')
-
-
 def check_positive_integer(value):
     ivalue = int(value)
     if ivalue <= 0:
