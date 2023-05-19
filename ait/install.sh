f4a70c5eed3289cb795637f9ccb62faa4286627b
hh8261 <8514919+hhsoul@user.noreply.gitee.com>
2023-05-19 02:22:38 +0000

    修改profile/benchmark为benchmark
    Signed-off-by: hh8261 <>

--- ait/install.sh
+++ ait/install.sh
@@ -3,7 +3,7 @@
 CURRENT_DIR=$(dirname $(readlink -f $0))
 arg_force_reinstall=
 only_debug=
-only_profile=
+only_benchmark=
 only_analyze=
 only_transplt=
 arg_help=0
@@ -12,7 +12,7 @@ while [[ "$#" -gt 0 ]]; do case $1 in
   --force-reinstall) arg_force_reinstall=--force-reinstall;;
   -f) arg_force_reinstall=--force-reinstall;;
   --debug) only_debug=true;;
-  --profile) only_profile=true;;
+  --benchmark) only_benchmark=true;;
   --analyze) only_analyze=true;;
   --transplt) only_transplt=true;;
   -h|--help) arg_help=1;;
@@ -34,7 +34,7 @@ if [ "$arg_help" -eq "1" ]; then
   echo "Usage: $0 [options]"
   echo " --help or -h      : Print help menu"
   echo " --debug : only install debug component"
-  echo " --profile : only install profile component"
+  echo " --benchmark : only install benchmark component"
   echo " --analyze : only install analyze component"
   echo " --transplt : only install transplt component"
   exit;
@@ -54,10 +54,10 @@ then
   ${arg_force_reinstall}
 fi
 
-if [ ! -z $only_profile ]
+if [ ! -z $only_benchmark ]
 then
-  pip install ${CURRENT_DIR}/components/profile/benchmark/backend \
-  ${CURRENT_DIR}/components/profile/benchmark \
+  pip install ${CURRENT_DIR}/components/benchmark/backend \
+  ${CURRENT_DIR}/components/benchmark \
   ${arg_force_reinstall}
 fi
 
@@ -73,12 +73,12 @@ then
   ${arg_force_reinstall}
 fi
 
-if [ -z $only_debug ] && [ -z $only_profile ] && [ -z $only_analyze ] && [ -z $only_transplt ]
+if [ -z $only_debug ] && [ -z $only_benchmark ] && [ -z $only_analyze ] && [ -z $only_transplt ]
 then
   pip install ${CURRENT_DIR}/components/debug/compare \
   ${CURRENT_DIR}/components/debug/surgeon \
-  ${CURRENT_DIR}/components/profile/benchmark/backend \
-  ${CURRENT_DIR}/components/profile/benchmark \
+  ${CURRENT_DIR}/components/benchmark/backend \
+  ${CURRENT_DIR}/components/benchmark \
   ${CURRENT_DIR}/components/analyze \
   ${CURRENT_DIR}/components/transplt \
   ${arg_force_reinstall}
