8e6027e7e482c78a3317cb9b7040e2b707e8a31a
yanhe13 <yanhe13@huawei.com>
2023-05-22 18:27:04 +0800

    debugv12

--- ait/components/benchmark/ais_bench/infer/benchmark_process.py
+++ ait/components/benchmark/ais_bench/infer/benchmark_process.py
@@ -50,10 +50,10 @@ def set_session_options(session, args):
         aipp_batchsize = args.batchsize
 
     # 确认模型只有一个动态 aipp input
-    if args.dym_shape is None:
-        aipp_input_exsity = session.get_dym_aipp_input_exsity()
-    else:
+    if args.dym_shape is not None:
         aipp_input_exsity = 0
+    else:
+        aipp_input_exsity = session.get_dym_aipp_input_exsity()
     logger.debug("aipp_input_exsity: {}".format(aipp_input_exsity))
     if (args.aipp_config is not None) and (aipp_input_exsity == 1):
         session.load_aipp_config_file(args.aipp_config, aipp_batchsize)
