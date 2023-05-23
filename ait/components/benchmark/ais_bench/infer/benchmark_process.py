02c483bda4f6e8b144284dfdbf71c1561e03140b
yanhe13 <yanhe13@huawei.com>
2023-05-23 11:25:09 +0800

    debugv14

--- ait/components/benchmark/ais_bench/infer/benchmark_process.py
+++ ait/components/benchmark/ais_bench/infer/benchmark_process.py
@@ -220,7 +220,7 @@ def infer_loop_array_run(session, args, intensors_desc, infileslist, output_pref
 
 def msprof_run_profiling(args, msprof_bin):
     cmd = sys.executable + " " + ' '.join(sys.argv) + " --profiler=0 --warmup-count=0"
-    msprof_cmd="{} --output={}/profiler --application=\"{}\" --model-execution=on \
+    msprof_cmd = "{} --output={}/profiler --application=\"{}\" --model-execution=on \
                     --sys-hardware-mem=on --sys-cpu-profiling=off --sys-profiling=off \
                     --sys-pid-profiling=off --dvpp-profiling=on --runtime-api=on --task-time=on --aicpu=on" \
                     .format(msprof_bin, args.output, cmd)
