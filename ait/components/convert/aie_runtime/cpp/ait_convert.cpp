2c2a1823ed6de0835a6e16870b3232b37de7f331
dragonzly <zhangyouling@huawei.com>
2023-05-26 22:08:38 +0800

    modify include files of ait_convert.cpp

--- ait/components/convert/aie_runtime/cpp/ait_convert.cpp
+++ ait/components/convert/aie_runtime/cpp/ait_convert.cpp
@@ -13,8 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+#include "AscendIE.h"
+#include <iostream>
 #include <fstream>
+
 using namespace AscendIE;
 
 int main(int argc, char** argv)
