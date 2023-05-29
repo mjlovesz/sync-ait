5bafce16b1de9a9e7724054f48d0f14f460bb64c
huanghao <huanghao122@huawei.com>
2023-05-29 12:10:45 +0800

    修改cleancode问题

--- ait/components/debug/compare/msquickcmp/onnx_model/onnx_dump_data.py
+++ ait/components/debug/compare/msquickcmp/onnx_model/onnx_dump_data.py
@@ -101,33 +101,6 @@ class OnnxDumpData(DumpData):
         self._save_dump_data(dump_bins, onnx_dump_data_dir, old_onnx_model, net_output_node)
         return onnx_dump_data_dir
 
-    def get_net_output_info(self):
-        """
-        get_net_output_info
-        """
-        return self.net_output
-
-    def generate_dump_data(self):
-        """
-        Function description:
-            generate onnx model dump data
-        Parameter:
-            none
-        Return Value:
-            onnx model dump data directory
-        Exception Description:
-            none
-        """
-        data_dir, onnx_dump_data_dir, model_dir = self._create_dir()
-        old_onnx_model, new_onnx_model_path = self._modify_model_add_outputs_nodes(model_dir)
-        session = self._load_session(new_onnx_model_path)
-        net_output_node = self._get_net_output_node()
-        inputs_tensor_info = self._get_inputs_tensor_info(session)
-        inputs_map = self._get_inputs_data(data_dir, inputs_tensor_info)
-        dump_bins = self._run_model(session, inputs_map)
-        self._save_dump_data(dump_bins, onnx_dump_data_dir, old_onnx_model, net_output_node)
-        return onnx_dump_data_dir
-
     def generate_dump_data_aipp(self, npu_dump_data_path):
         data_dir, onnx_dump_data_dir, model_dir = self._create_dir()
         old_onnx_model, new_onnx_model_path = self._modify_model_add_outputs_nodes(model_dir)
@@ -138,6 +111,12 @@ class OnnxDumpData(DumpData):
         dump_bins = self._run_model(session, inputs_map)
         self._save_dump_data(dump_bins, onnx_dump_data_dir, old_onnx_model, net_output_node)
         return onnx_dump_data_dir
+
+    def get_net_output_info(self):
+        """
+        get_net_output_info
+        """
+        return self.net_output
 
     def _create_dir(self):
         # create input directory
