9bc83758259121bf5063e8d64c12153b38c34577
yanhe13 <yanhe13@huawei.com>
2023-05-30 20:37:20 +0800

    aa

--- ait/components/debug/compare/tests/test_locate_accuracy_func.py
+++ ait/components/debug/compare/tests/test_locate_accuracy_func.py
@@ -2,4 +2,29 @@
 
 import pytest
 from msquickcmp.adapter_cli.args_adapter import CmpArgsAdapter
-from locat_accumalte_accuracy import find_accuracy_interval
+from msquickcmp.accuracy_locat.locat_accuracy import find_accuracy_interval
+
+
+class TestClass:
+    # staticmethod or classmethod
+
+    def init(self):
+        self.arg_1 = CmpArgsAdapter(
+                                    './output.onnx', # gold_model
+                                    './output.om', # om_model
+                                    "", # input_data_path
+                                    "/usr/local/Ascend/ascend-toolkit/latest/", # cann_path
+                                    '/root/miniconda3/envs/tjh/ait/ait/components/debug/compare/result/test/20230530181906/',
+                                    "",
+                                    "0",
+                                    "",
+                                    "",
+                                    False,
+                                    "",
+                                    True,
+                                    False
+        )
+
+
+
+    # testcases
