7d80d5ebd1316f2ee015d1a53bcebdeffbacd41d
root <yangchen160@huawei.com>
2023-05-31 17:32:56 +0800

    添加aipp输入参数说明和修改

--- ait/components/debug/compare/msquickcmp/onnx_model/onnx_dump_data.py
+++ ait/components/debug/compare/msquickcmp/onnx_model/onnx_dump_data.py
@@ -231,10 +231,10 @@ class OnnxDumpData(DumpData):
 
     def _get_inputs_data(self, data_dir, inputs_tensor_info, npu_dump_data_path, use_aipp):
         inputs_map = {}
-        if "" == self.args.input_path:
-            if use_aipp:
-                inputs_map = self._get_inputs_data_aipp(data_dir, inputs_tensor_info, npu_dump_data_path)
-            else:
+        if use_aipp:
+            inputs_map = self._get_inputs_data_aipp(data_dir, inputs_tensor_info, npu_dump_data_path)
+        else:
+            if "" == self.args.input_path:
                 for i, tensor_info in enumerate(inputs_tensor_info):
                     input_data = np.random.random(tensor_info["shape"]).astype(
                         self._convert_to_numpy_type(tensor_info["type"]))
@@ -243,19 +243,19 @@ class OnnxDumpData(DumpData):
                     input_data.tofile(os.path.join(data_dir, file_name))
                     utils.logger.info("save input file name: {}, shape: {}, dtype: {}".format(
                         file_name, input_data.shape, input_data.dtype))
-        else:
-            input_path = self.args.input_path.split(",")
-            if len(inputs_tensor_info) != len(input_path):
-                utils.logger.error("the number of model inputs tensor_info is not equal the number of "
-                                      "inputs data, inputs tensor_info is: {}, inputs data is: {}".format(
-                    len(inputs_tensor_info), len(input_path)))
-                raise AccuracyCompareException(utils.ACCURACY_COMPARISON_INVALID_DATA_ERROR)
-            for i, tensor_info in enumerate(inputs_tensor_info):
-                input_data = np.fromfile(input_path[i], self._convert_to_numpy_type(tensor_info["type"])).reshape(
-                    tensor_info["shape"])
-                inputs_map[tensor_info["name"]] = input_data
-                utils.logger.info("load input file name: {}, shape: {}, dtype: {}".format(
-                    os.path.basename(input_path[i]), input_data.shape, input_data.dtype))
+            else:
+                input_path = self.args.input_path.split(",")
+                if len(inputs_tensor_info) != len(input_path):
+                    utils.logger.error("the number of model inputs tensor_info is not equal the number of "
+                                        "inputs data, inputs tensor_info is: {}, inputs data is: {}".format(
+                        len(inputs_tensor_info), len(input_path)))
+                    raise AccuracyCompareException(utils.ACCURACY_COMPARISON_INVALID_DATA_ERROR)
+                for i, tensor_info in enumerate(inputs_tensor_info):
+                    input_data = np.fromfile(input_path[i], self._convert_to_numpy_type(tensor_info["type"])).reshape(
+                        tensor_info["shape"])
+                    inputs_map[tensor_info["name"]] = input_data
+                    utils.logger.info("load input file name: {}, shape: {}, dtype: {}".format(
+                        os.path.basename(input_path[i]), input_data.shape, input_data.dtype))
         return inputs_map
     
     def _get_inputs_data_aipp(self, data_dir, inputs_tensor_info, npu_dump_data_path):
