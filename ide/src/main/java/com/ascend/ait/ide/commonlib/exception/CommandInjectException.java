90abe54f23931748e2a425365d84a6e16bb80726
cabbage <chenxiaofeng45@huawei.com>
2023-05-31 14:55:38 +0800

    错误修改

--- ide/src/main/java/com/ascend/ait/ide/commonlib/exception/CommandInjectException.java
+++ ide/src/main/java/com/ascend/ait/ide/commonlib/exception/CommandInjectException.java
@@ -20,7 +20,6 @@ import com.ascend.ait.ide.commonlib.util.BundleUtil;
 
 public class CommandInjectException extends Exception {
     private static final long serialVersionUID = -2439139320983098242L;
-
     public CommandInjectException(String errorParam) {
         super(BundleUtil.getCommonlibsString("command.inject.error") + "The error param is " + errorParam);
     }
