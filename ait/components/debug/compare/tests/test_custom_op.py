2f1645d19088a5c353bf9446b18ebd1e348ec911
wujianwei.wu <wujianwei.wu@huawei.com>
2023-06-01 14:12:42 +0800

    删除

--- ait/components/debug/compare/tests/test_custom_op.py
+++ ait/components/debug/compare/tests/test_custom_op.py
@@ -50,6 +50,48 @@ def cmp_args() -> None:
                               custom_op="BatchMultiClassNMS_1203")
     yield args_adapter
 
+@pytest.fixture(scope="session", autouse=True)
+def fake_tmp_dir():
+    os.makedirs("./tmp", exist_ok=True)
+
+@pytest.fixture(scope="module", autouse=True)
+def fake_onnx_model(fake_tmp_dir):
+
+    import numpy as np
+    from auto_optimizer import OnnxGraph
+
+    # 加载 onnx 模型
+    g = OnnxGraph.parse('example/magic/layernorm.onnx')
+
+    # 增加一个整网输入节点
+    dummy_input = g.add_input('dummy_input', 'int32', [2, 3, 4])
+
+    # 增加一个 add 算子节点和一个 const 常量节点
+    add = g.add_node('dummy_add', 'Add')
+    add_ini = g.add_initializer('add_ini', np.array([[2, 3, 4]]))
+    add.inputs = ['dummy_input', 'add_ini'] # 手动连边
+    add.outputs = ['add_out']
+    g.update_map() # 手动连边后需更新连边关系
+
+
+    # 在 add 算子节点前插入一个 argmax 节点
+    argmax = g.add_node('dummy_ArgMax',
+                        'ArgMax',
+                        attrs={'axis': 0, 'keepdims': 1, 'select_last_index': 0})
+    g.insert_node('dummy_add', argmax, mode='before') # 由于 argmax 为单输入单输出节点，可以不手动连边而是使用 insert 函数
+
+    # 保存修改好的 onnx 模型
+    g.save('layernorm_modify.onnx')
+
+    # 切分子图
+    g.extract_subgraph(
+        "sub.onnx", 
+        ["start_node_name1", "start_node_name2"],
+        ["end_node_name1", "end_node_name1"],
+        input_shape="input1:1,3,224,224;input2:1,3,64,64",
+        input_dtype="input1:float16;input2:int8"
+    )
+
 
 def test_init_onnx_dump_data(cmp_args):
 
