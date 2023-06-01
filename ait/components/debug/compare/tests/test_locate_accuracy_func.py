75d8033ec23736eb3316558b0079daa0aacf78e6
yanhe13 <yanhe13@huawei.com>
2023-06-01 13:07:56 +0800

    finish mergev1

--- ait/components/debug/compare/tests/test_locate_accuracy_func.py
+++ ait/components/debug/compare/tests/test_locate_accuracy_func.py
@@ -16,7 +16,7 @@ import sys
 import os
 import pytest
 from msquickcmp.adapter_cli.args_adapter import CmpArgsAdapter
-from msquickcmp.accuracy_locat.locat_accuracy import find_accuracy_interval
+from msquickcmp.accuracy_locat.accuracy_locat import find_accuracy_interval
 
 logging.basicConfig(stream = sys.stdout, level = logging.INFO, format = '[%(levelname)s] %(message)s')
 logger = logging.getLogger(__name__)
