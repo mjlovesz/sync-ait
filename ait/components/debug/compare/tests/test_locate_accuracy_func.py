1e95f35abe7e033f2b2ba8d6952aa44e599635f3
yanhe13 <yanhe13@huawei.com>
2023-06-01 12:02:43 +0800

    om onnx dirv3

--- ait/components/debug/compare/tests/test_locate_accuracy_func.py
+++ ait/components/debug/compare/tests/test_locate_accuracy_func.py
@@ -27,7 +27,7 @@ class TestClass:
     @classmethod
     def get_base_path(cls):
         _current_dir = os.path.dirname(os.path.realpath(__file__))
-        return os.path.join(_current_dir, "../test/testdata/")
+        return os.path.join(_current_dir, "../test")
 
     @classmethod
     def setup_class(cls):
