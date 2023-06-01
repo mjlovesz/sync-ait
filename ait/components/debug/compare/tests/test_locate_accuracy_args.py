8ceb192d7a722eba2a94b7c5f74af521abaa212b
yanhe13 <yanhe13@huawei.com>
2023-06-01 10:48:38 +0800

    get model bash

--- ait/components/debug/compare/tests/test_locate_accuracy_args.py
+++ ait/components/debug/compare/tests/test_locate_accuracy_args.py
@@ -1,4 +1,16 @@
-
+# Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import logging
 import sys
 import os
@@ -10,3 +22,4 @@ logging.basicConfig(stream = sys.stdout, level = logging.INFO, format = '[%(leve
 logger = logging.getLogger(__name__)
 
 
+# 待完善
