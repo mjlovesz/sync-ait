d117e7d9d2378a61b512b8ff162cc7943b2ebecd
wujianwei.wu <wujianwei.wu@huawei.com>
2023-06-05 11:52:24 +0800

    aipp

--- ait/components/debug/compare/tests/test_aipp.py
+++ ait/components/debug/compare/tests/test_aipp.py
@@ -18,7 +18,6 @@ import stat
 
 import pytest
 import torch
-import acl
 
 from msquickcmp.common import utils
 from msquickcmp.common.utils import parse_input_shape_to_list
@@ -111,11 +110,9 @@ def fake_switch_config(fake_tmp_dir):
 
 @pytest.fixture(scope="module", autouse=True)
 def fake_om_model(fake_onnx_model, fake_aipp_config, fake_switch_config):
-    cmd = 'atc --model ./tmp/fake.onnx --soc_version '  + acl.get_soc_name() + \
-        '--framework 5 --input_format NCHW --input_shape image:1,3,7,7 --output ./tmp/fake --insert_op_conf' + \
-        './tmp/aipp.config --fusion_switch_file ./tmp/fusionswitch.cfg'
-
-    subprocess.run(cmd.split(), shell=False)
+    subprocess.run('atc --model ./tmp/fake.onnx --soc_version Ascend310P3 --framework 5 \
+    --input_format NCHW --input_shape image:1,3,7,7 --output ./tmp/fake --insert_op_conf \
+    ./tmp/aipp.config --fusion_switch_file ./tmp/fusionswitch.cfg'.split(), shell=False)
     return "./tmp/fake.om"
 
 
