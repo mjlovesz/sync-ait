45361278caca2dc4d383a7c1cb02bf4aacb901c4
qianzhengxin <qianzhengxin@huawei.com>
2023-06-12 14:28:58 +0800

    utils almost done

--- ait/components/benchmark/backend/concur/utils.h
+++ ait/components/benchmark/backend/concur/utils.h
@@ -9,7 +9,12 @@ using Arguments = std::unordered_map<std::string, std::string>;
 void readArgs(int argc, char *argv[], Arguments& arguments);
 std::stirng merge(std::vector<std::string> list, std::string delimiter); // merge a vector of string with delimiter
 std::vector<size_t> strVecToNumVec(const std::vector<std::string>& vec);
-std::vector<std::string> traversal(const char* dir);
+std::vector<std::string> traversal(const char* dir); // traversal a directory return vector of filename
+int createFilesList(std::vector<std::vecotr<std::string>>& fileList, std::string input);
+std::string getPrefix(std::string filePath);
+std::string removeSlash(std::string name);
+std::string createDynamicShape(std::string name, std::vector<size_t> shapes);
+
 
 
 
