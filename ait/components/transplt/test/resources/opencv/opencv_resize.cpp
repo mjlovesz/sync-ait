d7c7cdc9003dcd7e2ed78b508a131e8f917ca7e1
jinfaz <zhujinfa@huawei.com>
2023-06-13 15:27:59 +0800

    add st

--- ait/components/transplt/test/resources/opencv/opencv_resize.cpp
+++ ait/components/transplt/test/resources/opencv/opencv_resize.cpp
@@ -1,3 +1,15 @@
+//Copyright (c) 2023-2023 Huawei Technologies Co., Ltd.
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
 #include "opencv2/highgui.hpp"
 #include "opencv2/imgproc.hpp"
 
@@ -11,8 +23,10 @@ void resize_opencv(std::string imgFile) {
     cv::imwrite("./opencv_resize_output.jpg", resizedImg);
 }
 
+const int NUM_REQUIRED_ARGS = 2;
+
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
+    if (argc != NUM_REQUIRED_ARGS) {
         std::cout << "Usage ./resize_opencv xxx.jpg" << std::endl;
         return 1;
     }
