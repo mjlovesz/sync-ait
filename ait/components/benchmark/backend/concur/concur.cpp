647c0e20969035f866cb9c636882af2e39e43033
qianzhengxin <qianzhengxin@huawei.com>
2023-06-13 14:46:55 +0800

    cleancode

--- ait/components/benchmark/backend/concur/concur.cpp
+++ ait/components/benchmark/backend/concur/concur.cpp
@@ -46,53 +46,53 @@ using TimePointPair = std::pair<chr::steady_clock::time_point, chr::steady_clock
 using Arguments = std::unordered_map<std::string, std::string>;
 
 struct Feeds {
-    std::shared_ptr<std::vector<std::string>> _output_names = nullptr;
-    std::shared_ptr<std::vector<Base::BaseTensor>> _inputs = nullptr;
-    std::shared_ptr<std::vector<Base::TensorBase>> _outputs = nullptr;
-    std::shared_ptr<std::vector<Base::MemoryData>> _memory = nullptr;
-    std::shared_ptr<std::vector<std::shared_ptr<cnpy::NpyArray>>> _arrayptr = nullptr;
-    std::string _autoDynamicShape = "";
-    std::string _output_prefix = "";
+    std::shared_ptr<std::vector<std::string>> outputNames = nullptr;
+    std::shared_ptr<std::vector<Base::BaseTensor>> inputs = nullptr;
+    std::shared_ptr<std::vector<Base::TensorBase>> outputs = nullptr;
+    std::shared_ptr<std::vector<Base::MemoryData>> memory = nullptr;
+    std::shared_ptr<std::vector<std::shared_ptr<cnpy::NpyArray>>> arrayPtr = nullptr;
+    std::string autoDynamicShape = "";
+    std::string outputPrefix = "";
 };
 
 template <typename T>
 class ConcurrentQueue {
 public:
-    ConcurrentQueue(int depth = 3): _depth(depth) {} explicit
+    ConcurrentQueue(int depth = 3): depth(depth) {} explicit
 
     T pop() {
-        std::unique_lock<std::mutex> mlock(_mtx);
-        while (_queue.empty()) {
-            _cond_var.wait(mlock);
+        std::unique_lock<std::mutex> mlock(mtx);
+        while (queue.empty()) {
+            condVar.wait(mlock);
         }
-        auto val = _queue.front();
-        _queue.pop();
+        auto val = queue.front();
+        queue.pop();
         mlock.unlock();
-        _cond_var.notify_one();
+        condVar.notify_one();
         return val;
     }
 
     void push (const T &item) {
-        std::unique_lock<std::mutex> mlock(_mtx);
-        while (_queue.size() >= _depth) {
-            _cond_var.wait(mlock);
+        std::unique_lock<std::mutex> mlock(mtx);
+        while (queue.size() >= depth) {
+            condVar.wait(mlock);
         }
-        _queue.push(item);
+        queue.push(item);
         mlock.unlock();
-        _cond_var.notify_one();
+        condVar.notify_one();
     }
 
 private:
-    std::mutex _mtx;
-    std::queue<T> _queue;
-    std::condition_variable _cond_var;
-    int _depth;
+    std::mutex mtx;
+    std::queue<T> queue;
+    std::condition_variable condVar;
+    int depth;
 };
 
-void setSession(std::shared_ptr<Base::PyInferenceSession> session, Arguments& arguments)
+void SetSession(std::shared_ptr<Base::PyInferenceSession> session, Arguments& arguments)
 {
     if (arguments["dymHW"] != "") {
-        auto dymhw = split_str(arguments["dymHW"], ',');
+        auto dymhw = SplitStr(arguments["dymHW"], ',');
         session->SetDynamicHW(stoi(dymhw[0]), stoi(dymhw[1]));
     }
     if (arguments["dymDims"] != "") {
@@ -102,60 +102,63 @@ void setSession(std::shared_ptr<Base::PyInferenceSession> session, Arguments& ar
         session ->SetDynamicShape(arguments["dymShape"]);
     }
     if (arguments["outputSize"] != "") {
-        auto outputSize = strVecToNumVec(split_str(arguments["outputSize"], ','));
+        auto outputSize = StrVecToNumVec(SplitStr(arguments["outputSize"], ','));
         session->SetCustomOutTensorsSize(outputSize);
     }
 }
 
-void func_prepare(int32_t deviceId, std::shared_ptr<Base::PyInferenceSession> session, std::string modelPath,
+void FuncPrepare(int32_t deviceId, std::shared_ptr<Base::PyInferenceSession> session, std::string modelPath,
                   std::shared_ptr<Base::SessionOptions> options, std::vector<std::vector<std::string>> &filesList,
-                  ConcurrentQueue<std::shared_ptr<Feeds>> &h2d_queue, bool autoDymShape)
+                  ConcurrentQueue<std::shared_ptr<Feeds>> &h2dQueue, bool autoDymShape)
 {
     APP_ERROR ret;
     ret = Base::TensorContext::GetInstance()->SetContext(deviceId);
     if (ret != APP_ERR_OK) {
         throw std::runtime_error(GetError(ret));
     }
-    std::vector<std::string> input_names{};
+    std::vector<std::string> inputNames{};
     if (autoDymShape) {
         auto intensor_desc = session->GetInputs();
-        for (auto &desc: intensor_desc) {
-            input_names.push_back(desc.name);
+        for (auto &desc : intensor_desc) {
+            inputNames.push_back(desc.name);
         }
     }
     
-    for (auto &files: filesList) {
-        auto output_names = std::make_shared<std::vector<std::string>>();
+    for (auto &files : filesList) {
+        auto outputNames = std::make_shared<std::vector<std::string>>();
         for (const auto &desc: session->GetOutputs()) {
-            output_names->push_back(desc.name);
+            outputNames->push_back(desc.name);
         }
         auto inputs = std::make_shared<std::vector<Base::BaseTensor>>();
-        auto arrayptr = std::make_shared<std::vector<std::shared_ptr<cnpy::NpyArray>>>();
+        auto arrayPtr = std::make_shared<std::vector<std::shared_ptr<cnpy::NpyArray>>>();
         std::string autoDynamicShape{};
         for (size_t i = 0; i < files.size(); i++) {
-            auto array = std::make_shared<cnpy::NpyArray>(cnpy::npy_load(files[i]));
-            arrayptr->emplace_back(array);
-            inputs->emplace_back(array->data<void>(), array->num_bytes());
-            if (i != files.size()-1) {
-                autoDynamicShape += ";";
+            auto array = std::make_shared<cnpy::NpyArray>(cnpy::NpyLoad(files[i]));
+            arrayPtr->emplace_back(array);
+            inputs->emplace_back(array->Data<void>(), array->NumBytes());
+            if (autoDymShape) {
+                autoDynamicShape += CreateDynamicShape(inputNames[i], array->shape);
+                if (i != files.size()-1) {
+                    autoDynamicShape += ";";
+                }
             }
         }
-        auto output_prefix = getPrefix(files.front());
+        auto outputPrefix = GetPrefix(files.front());
         auto feeds = std::make_shared<Feeds>();
-        feeds->_autoDynamicShape = autoDynamicShape;
-        feeds->_arrayptr = arrayptr;
-        feeds->_output_names = output_names;
-        feeds->_inputs = inputs;
-        feeds->_output_prefix = output_prefix;
-        h2d_queue.push(feeds);
+        feeds->autoDynamicShape = autoDynamicShape;
+        feeds->arrayPtr = arrayPtr;
+        feeds->outputNames = outputNames;
+        feeds->inputs = inputs;
+        feeds->outputPrefix = outputPrefix;
+        h2dQueue.push(feeds);
     }
-    h2d_queue.push(nullptr);
+    h2dQueue.push(nullptr);
 }
 
-void func_h2d(ConcurrentQueue<std::shared_ptr<Feeds>> &h2d_queue,
-              ConcurrentQueue<std::shared_ptr<Feeds>> &compute_queue, int32_t deviceId,
+void FuncH2d(ConcurrentQueue<std::shared_ptr<Feeds>> &h2dQueue,
+              ConcurrentQueue<std::shared_ptr<Feeds>> &computeQueue, int32_t deviceId,
               std::shared_ptr<Base::PyInferenceSession> session,
-              std::vector<TimePointPair>& timestamps)
+              std::vector<TimePointPair>& timeStamps)
 {
     APP_ERROR ret;
     ret = Base::TensorContext::GetInstance()->SetContext(deviceId);
@@ -164,33 +167,33 @@ void func_h2d(ConcurrentQueue<std::shared_ptr<Feeds>> &h2d_queue,
     }
     while (true)
     {
-        auto item = h2d_queue.pop();
+        auto item = h2dQueue.pop();
         if (!item) {
-            compute_queue.push(nullptr);
+            computeQueue.push(nullptr);
             break;
         }
         auto start = chr::steady_clock::now();
 
-        item->_memory = std::make_shared<std::vector<Base::MemoryData>>();
+        item->memory = std::make_shared<std::vector<Base::MemoryData>>();
         auto inputs = std::make_shared<std::vector<Base::BaseTensor>>();
-        for (auto &info: *(item->_inputs)) {
+        for (auto &info : *(item->inputs)) {
             Base::MemoryData mem = Base::CopyMemory2DeviceMemory(info.buf, info.size, deviceId);
-            item->_memory->push_back(mem);
+            item->memory->push_back(mem);
             Base::BaseTensor tensor(mem.ptrData, mem.size);
             inputs->push_back(tensor);
         }
-        item->_inputs = inputs;
+        item->inputs = inputs;
         auto end = chr::steady_clock::now();
-        timestamps.emplace_back(start, end);
+        timeStamps.emplace_back(start, end);
 
-        compute_queue.push(item);
+        computeQueue.push(item);
     }
 }
 
-void func_compute(ConcurrentQueue<std::shared_ptr<Feeds>> &compute_queue,
-                  ConcurrentQueue<std::shared_ptr<Feeds>> &d2h_queue, int32_t deviceId,
+void FuncCompute(ConcurrentQueue<std::shared_ptr<Feeds>> &computeQueue,
+                  ConcurrentQueue<std::shared_ptr<Feeds>> &d2hQueue, int32_t deviceId,
                   std::shared_ptr<Base::PyInferenceSession> session,
-                  std::vector<TimePointPair>& timestamps)
+                  std::vector<TimePointPair>& timeStamps)
 {
     APP_ERROR ret;
     ret = Base::TensorContext::GetInstance()->SetContext(deviceId);
@@ -200,30 +203,30 @@ void func_compute(ConcurrentQueue<std::shared_ptr<Feeds>> &compute_queue,
 
     while (true)
     {
-        auto item = compute_queue.pop();
+        auto item = computeQueue.pop();
         if (!item) {
-            d2h_queue.push(nullptr);
+            d2hQueue.push(nullptr);
             break;
         }
         auto start = chr::steady_clock::now();
         
-        if (item->_autoDynamicShape != "") {
-            session->SetDynamicShape(item->_autoDynamicShape);
+        if (item->autoDynamicShape != "") {
+            session->SetDynamicShape(item->autoDynamicShape);
         }
         auto outputs = std::make_shared<std::vector<Base::TensorBase>>();
-        session->PureInfer(*(item->_inputs), *(item->_output_names), *outputs);
-        item->_outputs = outputs;
+        session->PureInfer(*(item->inputs), *(item->outputNames), *outputs);
+        item->outputs = outputs;
 
         auto end = chr::steady_clock::now();
-        timestamps.emplace_back(start, end);
+        timeStamps.emplace_back(start, end);
 
-        d2h_queue.push(item);
+        d2hQueue.push(item);
     }
 }
 
-void func_d2h(ConcurrentQueue<std::shared_ptr<Feeds>> &d2h_queue,
-              ConcurrentQueue<std::shared_ptr<Feeds>> &save_queue, int32_t deviceId,
-              std::vector<TimePointPair>& timestamps)
+void FuncD2h(ConcurrentQueue<std::shared_ptr<Feeds>> &d2hQueue,
+              ConcurrentQueue<std::shared_ptr<Feeds>> &saveQueue, int32_t deviceId,
+              std::vector<TimePointPair>& timeStamps)
 {
     APP_ERROR ret;
     ret = Base::TensorContext::GetInstance()->SetContext(deviceId);
@@ -233,59 +236,59 @@ void func_d2h(ConcurrentQueue<std::shared_ptr<Feeds>> &d2h_queue,
 
     while (true)
     {
-        auto item = d2h_queue.pop();
+        auto item = d2hQueue.pop();
         if (!item) {
-            save_queue.push(nullptr);
+            saveQueue.push(nullptr);
             break;
         }
         auto start = chr::steady_clock::now();
         
-        for (auto &output: *(item->_outputs)) {
+        for (auto &output : *(item->outputs)) {
             Base::TensorToHost(output);
         }
 
         auto end = chr::steady_clock::now();
-        timestamps.emplace_back(start, end);
+        timeStamps.emplace_back(start, end);
 
-        save_queue.push(item);
+        saveQueue.push(item);
     }
 }
 
-int tensotToNumpy(std::string output_filename, Base::TensorBase& output)
+int TensotToNumpy(std::string outputFileName, Base::TensorBase& output)
 {
-    auto shape_tmp = output.GetShape();
-    std::vector<size_t> shape{ shape_tmp.begin(), shape_tmp.end() };
+    auto shapeTmp = output.GetShape();
+    std::vector<size_t> shape { shapeTmp.begin(), shapeTmp.end() };
 
     if (output.GetDataType() == Base::TENSOR_DTYPE_FLOAT32) {
-        cnpy::npy_save(output_filename, (float*)output.GetBuffer(), shape);
+        cnpy::NpySave(outputFileName, (float*)output.GetBuffer(), shape);
     } else if (output.GetDataType() == Base::TENSOR_DTYPE_FLOAT16) {
-        cnpy::npy_save(output_filename, (short*)output.GetBuffer(), shape);
+        cnpy::NpySave(outputFileName, (aclFloat16*)output.GetBuffer(), shape);
     } else if (output.GetDataType() == Base::TENSOR_DTYPE_INT8) {
-        cnpy::npy_save(output_filename, (int8_t*)output.GetBuffer(), shape);
+        cnpy::NpySave(outputFileName, (int8_t*)output.GetBuffer(), shape);
     } else if (output.GetDataType() == Base::TENSOR_DTYPE_INT32) {
-        cnpy::npy_save(output_filename, (int32_t*)output.GetBuffer(), shape);
+        cnpy::NpySave(outputFileName, (int32_t*)output.GetBuffer(), shape);
     } else if (output.GetDataType() == Base::TENSOR_DTYPE_UINT8) {
-        cnpy::npy_save(output_filename, (uint8_t*)output.GetBuffer(), shape);
+        cnpy::NpySave(outputFileName, (uint8_t*)output.GetBuffer(), shape);
     } else if (output.GetDataType() == Base::TENSOR_DTYPE_INT16) {
-        cnpy::npy_save(output_filename, (int16_t*)output.GetBuffer(), shape);
+        cnpy::NpySave(outputFileName, (int16_t*)output.GetBuffer(), shape);
     } else if (output.GetDataType() == Base::TENSOR_DTYPE_UINT16) {
-        cnpy::npy_save(output_filename, (uint16_t*)output.GetBuffer(), shape);
+        cnpy::NpySave(outputFileName, (uint16_t*)output.GetBuffer(), shape);
     } else if (output.GetDataType() == Base::TENSOR_DTYPE_UINT32) {
-        cnpy::npy_save(output_filename, (uint32_t*)output.GetBuffer(), shape);
+        cnpy::NpySave(outputFileName, (uint32_t*)output.GetBuffer(), shape);
     } else if (output.GetDataType() == Base::TENSOR_DTYPE_INT64) {
-        cnpy::npy_save(output_filename, (int64_t*)output.GetBuffer(), shape);
+        cnpy::NpySave(outputFileName, (int64_t*)output.GetBuffer(), shape);
     } else if (output.GetDataType() == Base::TENSOR_DTYPE_UINT64) {
-        cnpy::npy_save(output_filename, (uint64_t*)output.GetBuffer(), shape);
+        cnpy::NpySave(outputFileName, (uint64_t*)output.GetBuffer(), shape);
     } else if (output.GetDataType() == Base::TENSOR_DTYPE_DOUBLE64) {
-        cnpy::npy_save(output_filename, (double*)output.GetBuffer(), shape);
+        cnpy::NpySave(outputFileName, (double*)output.GetBuffer(), shape);
     } else if (output.GetDataType() == Base::TENSOR_DTYPE_BOOL) {
-        cnpy::npy_save(output_filename, (bool*)output.GetBuffer(), shape);
+        cnpy::NpySave(outputFileName, (bool*)output.GetBuffer(), shape);
     }
     return 0;
 }
 
 
-void func_save(ConcurrentQueue<std::shared_ptr<Feeds>> &save_queue, int32_t deviceId, std::string output_dir)
+void FuncSave(ConcurrentQueue<std::shared_ptr<Feeds>> &saveQueue, int32_t deviceId, std::string outputDir)
 {
     APP_ERROR ret;
     ret = Base::TensorContext::GetInstance()->SetContext(deviceId);
@@ -295,21 +298,21 @@ void func_save(ConcurrentQueue<std::shared_ptr<Feeds>> &save_queue, int32_t devi
 
     while (true)
     {
-        auto item = save_queue.pop();
+        auto item = saveQueue.pop();
         if (!item) {
             break;
         }
-        for (auto &mem: *(item->_memory)) {
+        for (auto &mem : *(item->memory)) {
             Base::MemoryHelper::Free(mem);
         }
 
-        if (output_dir != "") {
-            size_t n = item->_outputs->size();
+        if (outputDir != "") {
+            size_t n = item->outputs->size();
             for (size_t i = 0; i < n; i++) {
-                std::string output_filename = output_dir + removeSlash(
-                    item->_output_prefix + "_" + item->_output_names->at(i) + "_" + std::to_string(i) + ".npy");
-                if (tensotToNumpy(output_filename, item->_outputs->at(i))) {
-                    std::cout << output_filename << "save failed" << endl;
+                std::string outputFileName = outputDir + RemoveSlash(
+                    item->outputPrefix + "_" + item->outputNames->at(i) + "_" + std::to_string(i) + ".npy");
+                if (TensotToNumpy(outputFileName, item->outputs->at(i))) {
+                    std::cout << outputFileName << "save failed" << endl;
                 }
             }
         }
@@ -320,7 +323,7 @@ void Execute(Arguments& arguments)
 {
     std::string input = arguments["input"];
     std::vector<std::vector<std::string>> filesList{};
-    createFilesList(filesList, input);
+    CreateFilesList(filesList, input);
 
     std::shared_ptr<Base::SessionOptions> options = std::make_shared<Base::SessionOptions>();
 
@@ -329,24 +332,26 @@ void Execute(Arguments& arguments)
     size_t deviceId = stoi(arguments["device"]);
     
     auto session = std::make_shared<Base::PyInferenceSession>(arguments["model"], deviceId, options);
-    setSession(session, arguments);
+    SetSession(session, arguments);
 
-    ConcurrentQueue<std::shared_ptr<Feeds>> h2d_queue;
-    ConcurrentQueue<std::shared_ptr<Feeds>> compute_queue;
-    ConcurrentQueue<std::shared_ptr<Feeds>> d2h_queue;
-    ConcurrentQueue<std::shared_ptr<Feeds>> save_queue;
+    ConcurrentQueue<std::shared_ptr<Feeds>> h2dQueue;
+    ConcurrentQueue<std::shared_ptr<Feeds>> computeQueue;
+    ConcurrentQueue<std::shared_ptr<Feeds>> d2hQueue;
+    ConcurrentQueue<std::shared_ptr<Feeds>> saveQueue;
 
-    std::vector<TimePointPair> h2d_ts;
-    std::vector<TimePointPair> compute_ts;
-    std::vector<TimePointPair> d2h_ts;
+    std::vector<TimePointPair> h2dTs;
+    std::vector<TimePointPair> computeTs;
+    std::vector<TimePointPair> d2hTs;
 
     auto start = chr::steady_clock::now();
 
-    std::thread h2dThread(func_h2d, std::ref(h2d_queue), std::ref(compute_queue), deviceId, session, std::ref(h2d_ts));
-    std::thread computeThread(func_compute, std::ref(compute_queue), std::ref(d2h_queue), deviceId, session, std::ref(compute_ts));
-    std::thread d2hThread(func_d2h, std::ref(d2h_queue), std::ref(save_queue), deviceId, std::ref(h2d_ts));
-    std::thread saveThread(func_save, std::ref(save_queue), deviceId, arguments["output"]);
-    func_prepare(deviceId, session, arguments["model"], options, filesList, h2d_queue, arguments["auto_set_dymshape_mode"] != "0");
+    std::thread h2dThread(FuncH2d, std::ref(h2dQueue), std::ref(computeQueue), deviceId, session, std::ref(h2dTs));
+    std::thread computeThread(FuncCompute, std::ref(computeQueue), std::ref(d2hQueue),
+                              deviceId, session, std::ref(computeTs));
+    std::thread d2hThread(FuncD2h, std::ref(d2hQueue), std::ref(saveQueue), deviceId, std::ref(d2hTs));
+    std::thread saveThread(FuncSave, std::ref(saveQueue), deviceId, arguments["output"]);
+    FuncPrepare(deviceId, session, arguments["model"], options,
+                filesList, h2dQueue, arguments["auto_set_dymshape_mode"] != "0");
 
     h2dThread.join();
     computeThread.join();
@@ -354,12 +359,12 @@ void Execute(Arguments& arguments)
     saveThread.join();
 
     auto end = chr::steady_clock::now();
-    auto e2e_ms = chr::duration_cast<chr::milliseconds>(end - start).count();
-    std::cout << "End2End time: " << e2e_ms << "ms" << std::endl;
+    auto e2ems = chr::duration_cast<chr::milliseconds>(end - start).count();
+    std::cout << "End2End time: " << e2ems << "ms" << std::endl;
 
-    printTimeWall("h2d", h2d_ts);
-    printTimeWall("compute", compute_ts);
-    printTimeWall("d2h", d2h_ts);
+    PrintTimeWall("h2d", h2dTs);
+    PrintTimeWall("compute", computeTs);
+    PrintTimeWall("d2h", d2hTs);
 
     session->Finalize();
 }
@@ -370,7 +375,7 @@ int main(int argc, char **argv) {
                     {"device", ""}, {"dymHW", ""}, {"dymDims", ""}, {"dymShape", ""}, {"display", "0"},
                     {"outputSize", ""}, {"auto_set_dymshape_mode", "0"}};
 
-    readArgs(argc, argv, arguments);
+    ReadArgs(argc, argv, arguments);
 
     Execute(arguments);
 }
