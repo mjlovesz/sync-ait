5abe4b833403c4ff3ca16b922905c79e0978cb22
yanhe13 <yanhe13@huawei.com>
2023-06-28 11:06:32 +0800

    benchmark true

--- ait/components/benchmark/ais_bench/infer/__main__.py
+++ ait/components/benchmark/ais_bench/infer/__main__.py
@@ -75,8 +75,8 @@ def check_device_range_valid(value):
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--modell",
-        "-ml",
+        "--model",
+        "-m",
         required=True,
         help="The path of the om model"
     )
