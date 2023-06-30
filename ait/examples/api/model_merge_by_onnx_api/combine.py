e970123abf5c26d7c3f398e4ebcfc60eecbe5fde
yaohan404 <yaohan8@huawei.com>
2023-06-30 13:18:45 +0800

    add readme of onnx combine

--- ait/examples/api/model_merge_by_onnx_api/combine.py
+++ ait/examples/api/model_merge_by_onnx_api/combine.py
@@ -600,12 +600,12 @@ def add_prefix(
 
 
 logging.basicConfig(level=logging.INFO)
-parser = argparse.ArgumentParser(description="merge two ONNX model into one")
+parser = argparse.ArgumentParser(description="Merge two ONNX model into one")
 parser.add_argument("--previous_model_path", type=str, help="Path to the previous ONNX model")
 parser.add_argument("--following_model_path", type=str, help="Path to the following ONNX model")
 parser.add_argument("--merge_model_path", type=str, help="Path to the merged ONNX model")
-parser.add_argument("--previous_model_outputs", type=str, nargs='+', help="output name of previous model")
-parser.add_argument("--following_model_inputs", type=str, nargs='+', help="input name of following model")
+parser.add_argument("--previous_model_outputs", type=str, nargs='+', help="Output name of previous model")
+parser.add_argument("--following_model_inputs", type=str, nargs='+', help="Input name of following model")
 
 args = parser.parse_args()
 previous_model_path = args.previous_model_path
