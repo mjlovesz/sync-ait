ba294238e35334ddd21870c165510c46ea67ca38
yanhe13 <yanhe13@huawei.com>
2023-07-06 14:57:12 +0800

    pipeline leadendv7

--- ait/components/benchmark/ais_bench/infer/io_oprations.py
+++ ait/components/benchmark/ais_bench/infer/io_oprations.py
@@ -105,7 +105,7 @@ def get_strings_from_files_list(files_list, pure_data_type):
         else:
             npath = file_path
         npathlist.append(npath)
-        return npathlist
+    return npathlist
 
 
 # get tensors from files list combile all files
