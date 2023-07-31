bb4ee323a8f5ea547cdf67fcc4783a58bbf67417
JacobHuang <huangjianbin1@huawei.com>
2023-07-31 23:08:06 +0800

    add pyacl sample

--- ait/components/benchmark/test/acl_infer_sample.py
+++ ait/components/benchmark/test/acl_infer_sample.py
@@ -1,4 +1,4 @@
-# Copyright 2020 Huawei Technologies Co., Ltd
+# Copyright 2023 Huawei Technologies Co., Ltd
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +13,7 @@
 # limitations under the License.
 # ============================================================================
 
-from pyacl.acl_infer import AclNet, init_acl, release_acl
+from ais_bench.infer.acl_infer import AclNet, init_acl, release_acl
 import acl
 import numpy as np
 
