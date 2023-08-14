d05c68c08ff1e907f897ce9c97f0f6e517dae65c
xiaolianzi_cl <chenlian16@huawei.com>
2023-08-14 19:18:07 +0800

    read acl dump tensor

--- ait/components/debug/compare/msquickcmp/pta_acl_cmp/compare.py
+++ ait/components/debug/compare/msquickcmp/pta_acl_cmp/compare.py
@@ -1,11 +1,15 @@
 import os
 
-import torch
-import csv
 import pandas as pd
 import numpy as np
 
 from msquickcmp.pta_acl_cmp.cmp_algorithm import cmp_alg_map
+
+ATTR_VERSION = "$Version"
+ATTR_END = "$End"
+ATTR_OBJECT_LENGTH = "$Object.Length"
+ATTR_OBJECT_COUNT = "$Object.Count"
+ATTR_OBJECT_PREFIX = "$Object."
 
 PTA = "pta"
 ACL = "acl"
@@ -19,7 +23,7 @@ CSV_HEADER = [DATA_ID, PTA_DATA_PATH, PTA_DTYPE, ACL_DATA_PATH, ACL_DTYPE, CMP_F
 CSV_HEADER.extend(list(cmp_alg_map.keys()))
 
 
-def set_label(data_src: str, data_id: str, data_val: torch.Tensor = None, tensor_path: str = None):
+def set_label(data_src: str, data_id: str, data_val=None, tensor_path=None):
     # task_id = os.getenv("CMP_TASK_ID")
     task_id = str(0)
     csv_dir = os.path.join(".", task_id)
@@ -116,5 +120,82 @@ def compare_tensor(csv_data: pd.DataFrame):
             csv_data[CMP_FLAG][idx] = True
     return csv_data
 
-def read_acl_transformer_data(data_path):
-    pass
+
+class TensorBinFile:
+    def __init__(self, file_path) -> None:
+        self.file_path = file_path
+        self.dtype = 0
+        self.format = 0
+        self.dims = []
+
+        self.__parse_bin_file()
+
+    def get_tensor(self):
+        if self.dtype == 0:
+            dtype = np.float32
+        elif self.dtype == 1:
+            dtype = np.float16
+        elif self.dtype == 2:  # int8
+            dtype = np.int8
+        elif self.dtype == 3:  # int32
+            dtype = np.int32
+        elif self.dtype == 9:  # int64
+            dtype = np.int64
+        elif self.dtype == 12:
+            dtype = np.bool8
+        else:
+            print("error, unsupport dtype:", self.dtype)
+            pass
+        tensor = torch.tensor(np.frombuffer(self.obj_buffer, dtype=dtype))
+        tensor = tensor.view(self.dims)
+        return tensor
+
+    def __parse_bin_file(self):
+        end_str = f"{ATTR_END}=1"
+        with open(self.file_path, "rb") as fd:
+            file_data = fd.read()
+
+            begin_offset = 0
+            for i in range(len(file_data)):
+                if file_data[i] == ord("\n"):
+                    line = file_data[begin_offset: i].decode("utf-8")
+                    begin_offset = i + 1
+                    fields = line.split("=")
+                    attr_name = fields[0]
+                    attr_value = fields[1]
+                    if attr_name == ATTR_END:
+                        self.obj_buffer = file_data[i + 1:]
+                        break
+                    elif attr_name.startswith("$"):
+                        self.__parse_system_atrr(attr_name, attr_value)
+                    else:
+                        self.__parse_user_attr(attr_name, attr_value)
+                        pass
+
+    def __parse_system_atrr(self, attr_name, attr_value):
+        if attr_name == ATTR_OBJECT_LENGTH:
+            self.obj_len = int(attr_value)
+        elif attr_name == ATTR_OBJECT_PREFIX:
+
+            pass
+
+    def __parse_user_attr(self, attr_name, attr_value):
+        if attr_name == "dtype":
+            self.dtype = int(attr_value)
+        elif attr_name == "format":
+            self.format = int(attr_value)
+        elif attr_name == "dims":
+            self.dims = attr_value.split(",")
+            for i in range(len(self.dims)):
+                self.dims[i] = int(self.dims[i])
+
+
+def read_acl_transformer_data(file_path):
+    if file_path.endswith(".bin"):
+        bin = TensorBinFile(file_path)
+        return bin.get_tensor()
+    else:
+        try:
+            return list(torch.load(file_path).state_dict().values())[0]
+        except:
+            return torch.load(file_path)
