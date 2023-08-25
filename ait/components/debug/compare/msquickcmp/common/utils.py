d3d6eae56b3f68f415058a973b02ad7f308731a9
王国伟 <wangguowei14@huawei.com>
2023-08-25 01:15:00 +0000

    !479 【compare】【security】在ATC 转 json 之后，直接打开文件，没有判断文件大小 * update

--- ait/components/debug/compare/msquickcmp/common/utils.py
+++ ait/components/debug/compare/msquickcmp/common/utils.py
@@ -68,6 +68,7 @@ DOT = "."
 ASCEND_BATCH_FIELD = "ascend_mbatch_batch_"
 BATCH_SCENARIO_OP_NAME = "{0}_ascend_mbatch_batch_{1}"
 INVALID_CHARS = ['|', ';', '&', '&&', '||', '>', '>>', '<', '`', '\\', '!', '\n']
+MAX_READ_FILE_SIZE_4G = 4294967296  # 4G, 4 * 1024 * 1024 * 1024
 
 
 class AccuracyCompareException(Exception):
@@ -111,15 +112,6 @@ def check_exec_cmd(command: str):
 
     return True
 
-def check_input_args(args: list):
-    for arg in args:
-        if arg in INVALID_CHARS:
-            logger.error("Arg {} is invalid.".format(arg))
-            return False
-
-    return True
-
-
 def check_exec_script_file(script_path: str):
     if not os.path.exists(script_path):
         logger.error("File {} is not exist.".format(script_path))
@@ -161,6 +153,139 @@ def check_file_or_directory_path(path, isdir=False):
             raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_PATH_ERROR)
 
 
+def check_input_bin_file_path(input_path):
+    """
+    Function Description:
+        check the output bin file
+    Parameter:
+        input_path: input path directory
+    """
+    input_bin_files = input_path.split(',')
+    bin_file_path_array = []
+    for input_item in input_bin_files:
+        input_item_path = os.path.realpath(input_item)
+        if input_item_path.endswith('.bin'):
+            check_file_or_directory_path(input_item_path)
+            bin_file_path_array.append(input_item_path)
+        else:
+            check_file_or_directory_path(input_item_path, True)
+            get_input_path(input_item_path, bin_file_path_array)
+    return bin_file_path_array
+
+
+def check_file_size_valid(file_path, size_max):
+    if os.stat(file_path).st_size > size_max:
+        logger.error(f'file_path={file_path} is too large, > {size_max}, not valid.')
+        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_DATA_ERROR)
+
+
+def check_input_args(args: list):
+    for arg in args:
+        if arg in INVALID_CHARS:
+            logger.error("Arg {} is invalid.".format(arg))
+            return False
+
+    return True
+
+
+def check_convert_is_valid_used(dump, bin2npy, custom_op):
+    """
+    check dump is True while using convert
+    """
+    if not dump and (bin2npy or custom_op != ""):
+        logger.error(
+            "Convert option or custom_op is forbidden when dump is False!\
+            Please keep dump True while using convert."
+        )
+        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_COMMAND_ERROR)
+
+
+def check_locat_is_valid(dump, locat):
+    """
+    Function:
+        check locat args is completed
+    Return:
+        True or False
+    """
+    if locat and not dump:
+            logger.error("Dump must be True when locat is used")
+            raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_COMMAND_ERROR)
+
+
+def check_device_param_valid(device):
+    """
+    check device param valid.
+    """
+    if not device.isdigit() or int(device) > MAX_DEVICE_ID:
+        logger.error(
+            "Please enter a valid number for device, the device id should be"
+            " in [0, 255], now is %s." % device)
+        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_DEVICE_ERROR)
+
+
+def check_dynamic_shape(shape):
+    """
+    Function Description:
+        check dynamic shpae
+    Parameter:
+        shape:shape
+    Return Value:
+        False or True
+    """
+    dynamic_shape = False
+    for item in shape:
+        if item is None or isinstance(item, str):
+            dynamic_shape = True
+            break
+    return dynamic_shape
+
+
+def _check_colon_exist(input_shape):
+    if ":" not in input_shape:
+        logger.error(get_shape_not_match_message(InputShapeError.FORMAT_NOT_MATCH, input_shape))
+        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_PARAM_ERROR)
+
+
+def _check_content_split_length(content_split):
+    if not content_split[1]:
+        logger.error(get_shape_not_match_message(InputShapeError.VALUE_TYPE_NOT_MATCH, content_split[1]))
+        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_PARAM_ERROR)
+
+
+def _check_shape_number(input_shape_value, pattern=DIM_PATTERN):
+    dim_pattern = re.compile(pattern)
+    match = dim_pattern.match(input_shape_value)
+    if not match or match.group() is not input_shape_value:
+        logger.error(get_shape_not_match_message(InputShapeError.VALUE_TYPE_NOT_MATCH, input_shape_value))
+        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_PARAM_ERROR)
+
+
+def check_input_name_in_model(tensor_name_list, input_name):
+    """
+    Function Description:
+        check input name in model
+    Parameter:
+        tensor_name_list: the tensor name list
+        input_name: the input name
+    Exception Description:
+        When input name not in tensor name list throw exception
+    """
+    if input_name not in tensor_name_list:
+        logger.error(get_shape_not_match_message(InputShapeError.NAME_NOT_MATCH, input_name))
+        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_PARAM_ERROR)
+
+
+def check_max_size_param_valid(max_cmp_size):
+    """
+    check max_size param valid.
+    """
+    if max_cmp_size < 0:
+        logger.error(
+            "Please enter a valid number for max_cmp_size, the max_cmp_size should be"
+            " in [0, ∞), now is %s." % max_cmp_size)
+        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_DEVICE_ERROR)
+
+
 def get_model_name_and_extension(offline_model_path):
     """
     Function Description:
@@ -179,6 +304,14 @@ def get_model_name_and_extension(offline_model_path):
             offline_model_path))
         raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_PATH_ERROR)
     return model_name, extension
+
+
+def get_input_path(input_item_path, bin_file_path_array):
+    for root, _, files in os.walk(input_item_path):
+        for bin_file in files:
+            if bin_file.endswith('.bin'):
+                file_path = os.path.join(root, bin_file)
+                bin_file_path_array.append(file_path)
 
 
 def get_dump_data_path(dump_dir, is_net_output=False):
@@ -219,113 +352,71 @@ def get_dump_data_path(dump_dir, is_net_output=False):
     return dump_data_path, file_is_exist
 
 
-def execute_command(cmd, info_need=True):
+def get_shape_to_directory_name(input_shape):
+    shape_info = re.sub(r"[:;]", "-", input_shape)
+    shape_info = re.sub(r",", "_", shape_info)
+    return shape_info
+
+
+def get_shape_not_match_message(shape_error_type, value):
     """
     Function Description:
-        run the following command
+        get shape not match message
     Parameter:
-        cmd: command
+        input:the value
+        shape_error_type: the shape error type
     Return Value:
-        command output result
-    Exception Description:
-        when invalid command throw exception
+        not match message
     """
-    if info_need:
-        logger.info('Execute command:%s' % " ".join(cmd))
-    process = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    while process.poll() is None:
-        line = process.stdout.readline()
-        line = line.strip()
-    if process.returncode != 0:
-        logger.error('Failed to execute command:%s' % " ".join(cmd))
-        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_DATA_ERROR)
+    message = ""
+    if shape_error_type == InputShapeError.FORMAT_NOT_MATCH:
+        message = "Input shape \"{}\" format mismatch,the format like: " \
+                  "input_name1:1,224,224,3;input_name2:3,300".format(value)
+    if shape_error_type == InputShapeError.VALUE_TYPE_NOT_MATCH:
+        message = "Input shape \"{}\" value not number".format(value)
+    if shape_error_type == InputShapeError.NAME_NOT_MATCH:
+        message = "Input tensor name \"{}\" not in model".format(value)
+    if shape_error_type == InputShapeError.TOO_LONG_PARAMS:
+        message = "Input \"{}\" value too long".format(value)
+    return message
 
 
-def create_directory(dir_path):
-    """
-    Function Description:
-        creating a directory with specified permissions
-    Parameter:
-        dir_path: directory path
-    Exception Description:
-        when invalid data throw exception
-    """
-    if not os.path.exists(dir_path):
-        try:
-            os.makedirs(dir_path, mode=0o700)
-        except OSError as ex:
-            logger.error(
-                'Failed to create {}.Please check the path permission or disk space .{}'.format(dir_path, str(ex)))
-            raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_PATH_ERROR) from ex
+def get_batch_index(dump_data_path):
+    for _, _, files in os.walk(dump_data_path):
+        for file_name in files:
+            if ASCEND_BATCH_FIELD in file_name:
+                return get_batch_index_from_name(file_name)
+    return ""
 
 
-def get_input_path(input_item_path, bin_file_path_array):
-    for root, _, files in os.walk(input_item_path):
-        for bin_file in files:
-            if bin_file.endswith('.bin'):
-                file_path = os.path.join(root, bin_file)
-                bin_file_path_array.append(file_path)
+def get_mbatch_op_name(om_parser, op_name, npu_dump_data_path):
+    _, scenario = om_parser.get_dynamic_scenario_info()
+    if scenario in [DynamicArgumentEnum.DYM_BATCH, DynamicArgumentEnum.DYM_DIMS]:
+        batch_index = get_batch_index(npu_dump_data_path)
+        current_op_name = BATCH_SCENARIO_OP_NAME.format(op_name, batch_index)
+    else:
+        return op_name
+    return current_op_name
 
 
-def check_input_bin_file_path(input_path):
-    """
-    Function Description:
-        check the output bin file
-    Parameter:
-        input_path: input path directory
-    """
-    input_bin_files = input_path.split(',')
-    bin_file_path_array = []
-    for input_item in input_bin_files:
-        input_item_path = os.path.realpath(input_item)
-        if input_item_path.endswith('.bin'):
-            check_file_or_directory_path(input_item_path)
-            bin_file_path_array.append(input_item_path)
-        else:
-            check_file_or_directory_path(input_item_path, True)
-            get_input_path(input_item_path, bin_file_path_array)
-    return bin_file_path_array
+def get_batch_index_from_name(name):
+    batch_index = ""
+    last_batch_field_index = name.rfind(ASCEND_BATCH_FIELD)
+    pos = last_batch_field_index + len(ASCEND_BATCH_FIELD)
+    while pos < len(name) and name[pos].isdigit():
+        batch_index += name[pos]
+        pos += 1
+    return batch_index
 
 
-def check_dynamic_shape(shape):
-    """
-    Function Description:
-        check dynamic shpae
-    Parameter:
-        shape:shape
-    Return Value:
-        False or True
-    """
-    dynamic_shape = False
+def get_data_len_by_shape(shape):
+    data_len = 1
     for item in shape:
-        if item is None or isinstance(item, str):
-            dynamic_shape = True
-            break
-    return dynamic_shape
-
-
-def check_convert_is_valid_used(dump, bin2npy, custom_op):
-    """
-    check dump is True while using convert
-    """
-    if not dump and (bin2npy or custom_op != ""):
-        logger.error(
-            "Convert option or custom_op is forbidden when dump is False!\
-            Please keep dump True while using convert."
-        )
-        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_COMMAND_ERROR)
-
-
-def check_locat_is_valid(dump, locat):
-    """
-    Function:
-        check locat args is completed
-    Return:
-        True or False
-    """
-    if locat and not dump:
-            logger.error("Dump must be True when locat is used")
-            raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_COMMAND_ERROR)
+        if item == -1:
+            logger.warning("please check your input shape, one dim in shape is -1.")
+            return -1
+        data_len = data_len * item
+    return data_len
 
 
 def parse_input_shape(input_shape):
@@ -425,101 +516,6 @@ def parse_dym_shape_range(dym_shape_range):
     return res
 
 
-def get_shape_to_directory_name(input_shape):
-    shape_info = re.sub(r"[:;]", "-", input_shape)
-    shape_info = re.sub(r",", "_", shape_info)
-    return shape_info
-
-
-def _check_colon_exist(input_shape):
-    if ":" not in input_shape:
-        logger.error(get_shape_not_match_message(InputShapeError.FORMAT_NOT_MATCH, input_shape))
-        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_PARAM_ERROR)
-
-
-def _check_content_split_length(content_split):
-    if not content_split[1]:
-        logger.error(get_shape_not_match_message(InputShapeError.VALUE_TYPE_NOT_MATCH, content_split[1]))
-        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_PARAM_ERROR)
-
-
-def _check_shape_number(input_shape_value, pattern=DIM_PATTERN):
-    dim_pattern = re.compile(pattern)
-    match = dim_pattern.match(input_shape_value)
-    if not match or match.group() is not input_shape_value:
-        logger.error(get_shape_not_match_message(InputShapeError.VALUE_TYPE_NOT_MATCH, input_shape_value))
-        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_PARAM_ERROR)
-
-
-def get_shape_not_match_message(shape_error_type, value):
-    """
-    Function Description:
-        get shape not match message
-    Parameter:
-        input:the value
-        shape_error_type: the shape error type
-    Return Value:
-        not match message
-    """
-    message = ""
-    if shape_error_type == InputShapeError.FORMAT_NOT_MATCH:
-        message = "Input shape \"{}\" format mismatch,the format like: " \
-                  "input_name1:1,224,224,3;input_name2:3,300".format(value)
-    if shape_error_type == InputShapeError.VALUE_TYPE_NOT_MATCH:
-        message = "Input shape \"{}\" value not number".format(value)
-    if shape_error_type == InputShapeError.NAME_NOT_MATCH:
-        message = "Input tensor name \"{}\" not in model".format(value)
-    if shape_error_type == InputShapeError.TOO_LONG_PARAMS:
-        message = "Input \"{}\" value too long".format(value)
-    return message
-
-
-def check_input_name_in_model(tensor_name_list, input_name):
-    """
-    Function Description:
-        check input name in model
-    Parameter:
-        tensor_name_list: the tensor name list
-        input_name: the input name
-    Exception Description:
-        When input name not in tensor name list throw exception
-    """
-    if input_name not in tensor_name_list:
-        logger.error(get_shape_not_match_message(InputShapeError.NAME_NOT_MATCH, input_name))
-        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_PARAM_ERROR)
-
-
-def save_numpy_data(file_path, data):
-    """
-    save_numpy_data
-    """
-    if not os.path.exists(os.path.dirname(file_path)):
-        os.makedirs(os.path.dirname(file_path))
-    np.save(file_path, data)
-
-
-def check_device_param_valid(device):
-    """
-    check device param valid.
-    """
-    if not device.isdigit() or int(device) > MAX_DEVICE_ID:
-        logger.error(
-            "Please enter a valid number for device, the device id should be"
-            " in [0, 255], now is %s." % device)
-        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_DEVICE_ERROR)
-
-
-def check_max_size_param_valid(max_cmp_size):
-    """
-    check max_size param valid.
-    """
-    if max_cmp_size < 0:
-        logger.error(
-            "Please enter a valid number for max_cmp_size, the max_cmp_size should be"
-            " in [0, ∞), now is %s." % max_cmp_size)
-        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_DEVICE_ERROR)
-
-
 def parse_arg_value(values):
     """
     parse dynamic arg value of atc cmdline
@@ -546,22 +542,53 @@ def parse_value_by_comma(value):
     return value_list
 
 
-def get_batch_index(dump_data_path):
-    for _, _, files in os.walk(dump_data_path):
-        for file_name in files:
-            if ASCEND_BATCH_FIELD in file_name:
-                return get_batch_index_from_name(file_name)
-    return ""
+def execute_command(cmd, info_need=True):
+    """
+    Function Description:
+        run the following command
+    Parameter:
+        cmd: command
+    Return Value:
+        command output result
+    Exception Description:
+        when invalid command throw exception
+    """
+    if info_need:
+        logger.info('Execute command:%s' % " ".join(cmd))
+    process = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    while process.poll() is None:
+        line = process.stdout.readline()
+        line = line.strip()
+    if process.returncode != 0:
+        logger.error('Failed to execute command:%s' % " ".join(cmd))
+        raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_DATA_ERROR)
 
 
-def get_mbatch_op_name(om_parser, op_name, npu_dump_data_path):
-    _, scenario = om_parser.get_dynamic_scenario_info()
-    if scenario in [DynamicArgumentEnum.DYM_BATCH, DynamicArgumentEnum.DYM_DIMS]:
-        batch_index = get_batch_index(npu_dump_data_path)
-        current_op_name = BATCH_SCENARIO_OP_NAME.format(op_name, batch_index)
-    else:
-        return op_name
-    return current_op_name
+def create_directory(dir_path):
+    """
+    Function Description:
+        creating a directory with specified permissions
+    Parameter:
+        dir_path: directory path
+    Exception Description:
+        when invalid data throw exception
+    """
+    if not os.path.exists(dir_path):
+        try:
+            os.makedirs(dir_path, mode=0o700)
+        except OSError as ex:
+            logger.error(
+                'Failed to create {}.Please check the path permission or disk space .{}'.format(dir_path, str(ex)))
+            raise AccuracyCompareException(ACCURACY_COMPARISON_INVALID_PATH_ERROR) from ex
+
+
+def save_numpy_data(file_path, data):
+    """
+    save_numpy_data
+    """
+    if not os.path.exists(os.path.dirname(file_path)):
+        os.makedirs(os.path.dirname(file_path))
+    np.save(file_path, data)
 
 
 def handle_ground_truth_files(om_parser, npu_dump_data_path, golden_dump_data_path):
@@ -574,26 +601,6 @@ def handle_ground_truth_files(om_parser, npu_dump_data_path, golden_dump_data_pa
                 current_op_name = BATCH_SCENARIO_OP_NAME.format(file_name[:first_dot_index], batch_index)
                 dst_file_name = current_op_name + file_name[first_dot_index:]
                 shutil.copy(os.path.join(root, file_name), os.path.join(root, dst_file_name))
-
-
-def get_batch_index_from_name(name):
-    batch_index = ""
-    last_batch_field_index = name.rfind(ASCEND_BATCH_FIELD)
-    pos = last_batch_field_index + len(ASCEND_BATCH_FIELD)
-    while pos < len(name) and name[pos].isdigit():
-        batch_index += name[pos]
-        pos += 1
-    return batch_index
-
-
-def get_data_len_by_shape(shape):
-    data_len = 1
-    for item in shape:
-        if item == -1:
-            logger.warning("please check your input shape, one dim in shape is -1.")
-            return -1
-        data_len = data_len * item
-    return data_len
 
 
 def str2bool(v):
