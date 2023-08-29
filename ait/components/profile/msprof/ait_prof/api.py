ee864f406c2b304d195b38b5b49e1f15180b2e2b
binghamhuang <huangjianbin1@huawei.com>
2023-08-29 20:28:16 +0800

    add trex api

--- ait/components/profile/msprof/ait_prof/api.py
+++ ait/components/profile/msprof/ait_prof/api.py
@@ -22,12 +22,11 @@ def get_csv_to_df(file_path) -> any:
 
 
 # 返回在按指定列的最大值对 DataFrame 进行排序后从顶部开始，返回指定数量的行
-# List the k-slowest kernel
 def get_nlargest(df, n, columns, keep='last') -> any:
     return df.nlargest(n, columns, keep)
 
 
-# Compute the latency of the top-3 slowest layers:
+# 计算top-3的和:
 def get_top3_sum(df, columns, keep='last') -> any:
     top3 = get_nlargest(df, 3, columns, keep)
     return top3[columns].sum()
@@ -81,50 +80,50 @@ def get_smaller_value(df, label, value) -> any:
     return df.where(df[label] < value)
 
 
+# 返回满足col1列值条件的col2列最后一个元素值
 def get_item_value(df, col1, col1_value, col2) -> any:
-    '''返回满足col1列值条件的col2列最后一个元素值'''
     return df.at[df.index[df[col1] == col1_value].values[-1], col2]
 
 
+# 修改满足col1列值条件的col2列最后一个元素值
 def set_item_value(df, col1, col1_value, col2, col2_value) -> None:
-    '''修改满足col1列值条件的col2列最后一个元素值'''
     df.at[df.index[df[col1] == col1_value].values[-1], col2] = col2_value
 
 
+# 返回满足col1列值条件的col2列元素值列表
 def get_col_value(df, col1, col1_value, col2) -> any:
-    '''返回满足col1列值条件的col2列元素值列表'''
     return df.loc[df[col1] == col1_value, col2].values
 
 
+# 修改满足col1列值条件的col2列元素值
 def set_col_value(df, col1, col1_value, col2, col2_value) -> None:
-    '''修改满足col1列值条件的col2列元素值'''
     df.loc[df[col1] == col1_value, col2] = col2_value
 
 
+# 在末尾增加一行或多行记录
 def add_row(df, rows) -> any:
-    '''在末尾增加一行或多行记录'''
     return df.append(pd.DataFrame(rows), ignore_index=True)
 
 
+# 在满足col1列值条件的末尾插入行
 def insert_row_1(df, col, col_value, rows) -> any:
-    '''在满足col1列值条件的末尾插入行'''
     i = df.index[(df[col] == col_value)].values[-1]
     df_1 = df.loc[0:i, :].append(pd.DataFrame(rows), ignore_index=True)
     return df_1.append(df.loc[i+1:, :], ignore_index=True)
 
 
+# 在满足col1、col2两列值条件的末尾插入行
 def insert_row_2(df, col1, col1_value, col2, col2_value, rows) -> any:
-    '''在满足col1、col2两列值条件的末尾插入行'''
     i = df.index[(df[col1] == col1_value) & (df[col2] == col2_value)].values[-1]
     df_1 = df.loc[0:i, :].append(pd.DataFrame(rows), ignore_index=True)
     return df_1.append(df.loc[i+1:, :], ignore_index=True)
 
 
+# 删除指定某列值的行
 def drop_row_1(df, col, col_value) -> None:
-    '''删除指定某列值的行'''
     df.drop(df.index[(df[col] == col_value)], inplace=True)
 
 
+# 删除指定某两列值的行
 def drop_row_2(df, col1, col1_value, col2, col2_value) -> None:
-    '''删除指定某两列值的行'''
     df.drop(df.index[(df[col1] == col1_value) & (df[col2] == col2_value)], inplace=True)
