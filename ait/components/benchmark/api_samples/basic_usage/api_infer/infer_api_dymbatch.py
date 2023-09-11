8300143ab8ff94aaf854870bf1a73e232a27e46d
yanhe13 <yanhe13@huawei.com>
2023-09-11 11:36:47 +0800

    infer api sample

---
+++ ait/components/benchmark/api_samples/basic_usage/api_infer/infer_api_dymbatch.py
@@ -0,0 +1,26 @@
+import numpy as np
+from ais_bench.infer.interface import InferSession
+
+def infer_api_dymbatch():
+    device_id = 0
+    model_path = "../../sampledata/add_model/model/add_model_dymbatch.om"
+
+    # create session of om model for inference
+    session = InferSession(device_id, model_path)
+
+    # create new numpy data according inputs info
+    shape0 = [4, 3, 32, 32]
+    ndata0 = np.full(shape0, 1).astype(np.float32)
+    shape1 = [4, 3, 32, 32]
+    ndata1 = np.full(shape1, 1).astype(np.float32)
+    feeds = [ndata0, ndata1]
+
+    # execute inference, inputs is ndarray list and outputs is ndarray list
+    outputs = session.infer(feeds, mode='dymbatch')
+    print(f"outputs: {outputs}")
+
+    # free model resource and device context of session
+    session.free_device()
+
+
+infer_api_dymbatch()
