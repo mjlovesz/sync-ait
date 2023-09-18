dabb3e2f5d35729da8f39d6c7fbae45c9e3f3eae
chenlian <chenlian16@huawei.com>
2023-09-18 12:48:41 +0000

    !532 【compare】【aclcmp】【穿刺任务】基于weight的加速库半自动映射精度比对 * update * update * update * update * update * remove dump_tag * update * update * update * update * update * add file header * Merge branch 'pta_compare' of https://gitee.com/wangguowei33/ait into compare * remove unuse cod * fix bug * Merge branch 'compare' of https://gitee.com/xiaolianzi_cl/ait into compare * restruct pta_acl_cmp * Merge branch 'compare' of https://gitee.com/xiaolianzi_cl/ait into compare * delete unuse code * Merge branch 'compare' of https://gitee.com/xiaolianzi_cl/ait into compare * change .bin to .npy in set_label * Merge branch 'compare' of https://gitee.com/xiaolianzi_cl/ait into compare * fix read json bug * Merge branch 'compare' of https://gitee.com/xiaolianzi_cl/ait into compare * save cmp result to csv * Merge branch 'compare' of https://gitee.com/xiaolianzi_cl/ait into compare * add compare_metadata func * Merge branch 'master' of https://gitee.com/ascend/ait into compare * Merge branch 'compare' of https://gitee.com/xiaolianzi_cl/ait into compare * change metadata * Merge branch 'compare' of https://gitee.com/xiaolianzi_cl/ait into compare * pt hook * fix hook bug * Merge branch 'compare' of https://gitee.com/xiaolianzi_cl/ait into compare * add pt hook module * delete ptdbg * add ptdbg to msquickcmp

--- ait/components/debug/compare/msquickcmp/pta_acl_cmp/compare.py
+++ ait/components/debug/compare/msquickcmp/pta_acl_cmp/compare.py
@@ -19,16 +19,10 @@ import numpy as np
 import torch
 
 from msquickcmp.common.utils import logger
-from msquickcmp.pta_acl_cmp.cmp_algorithm import cmp_alg_map
-from msquickcmp.pta_acl_cmp.constant import ATTR_END, ATTR_OBJECT_LENGTH, ATTR_OBJECT_COUNT, \
-    ATTR_OBJECT_PREFIX, PTA, ACL, DATA_ID, PTA_DATA_PATH, ACL_DATA_PATH, PTA_DTYPE, PTA_SHAPE, \
-    PTA_MAX_VALUE, PTA_MIN_VALUE, PTA_MEAN_VALUE, PTA_STACK, ACL_DTYPE, ACL_SHAPE, ACL_MAX_VALUE, \
-    ACL_MIN_VALUE, ACL_MEAN_VALUE, ACL_STACK, CMP_FLAG, CMP_FAIL_REASON, CSV_HEADER, \
+from msquickcmp.pta_acl_cmp.constant import DATA_ID, PTA_DATA_PATH, ACL_DATA_PATH, \
+    CMP_FLAG, CSV_HEADER, \
     MODEL_INFER_TASK_ID, AIT_CMP_TASK_DIR, AIT_CMP_TASK, AIT_CMP_TASK_PID, ACL_DATA_MAP_FILE
-
-
-CSV_HEADER.extend(list(cmp_alg_map.keys()))
-CSV_HEADER.append(CMP_FAIL_REASON)
+from msquickcmp.pta_acl_cmp.utils import compare_tensor
 
 token_counts = 0
 
@@ -64,21 +58,17 @@ def save_pta_data(csv_data, data_id, data_val, data_path):
     data_val = data_val.cpu().numpy()
     mapping_data = csv_data[csv_data[DATA_ID] == data_id]
     if mapping_data.empty:
-        data_val.tofile(data_path)
+        np.save(data_path, data_val)
         row_data = pd.DataFrame({
             DATA_ID: [data_id],
             PTA_DATA_PATH: [data_path],
-            PTA_DTYPE: [str(data_val.dtype)],
-            PTA_SHAPE: [str(data_val.shape)],
             CMP_FLAG: [False]
         })
         csv_data = pd.concat([csv_data, row_data], ignore_index=True)
     else:
         index = mapping_data.index.values[0]
-        data_val.tofile(data_path)
+        np.save(data_path, data_val)
         csv_data[PTA_DATA_PATH][index] = data_path
-        csv_data[PTA_DTYPE][index] = str(data_val.dtype)
-        csv_data[PTA_SHAPE][index] = str(data_val.shape)
 
         # 对应的acl_data存在时，触发比对
         csv_data = compare_tensor(csv_data=csv_data)
@@ -93,21 +83,17 @@ def save_acl_data(csv_data, data_id, data_val, data_path):
     data_val = data_val.cpu().numpy()
     mapping_data = csv_data[csv_data[DATA_ID] == data_id]
     if mapping_data.empty:
-        data_val.tofile(data_path)
+        np.save(data_path, data_val)
         row_data = pd.DataFrame({
             DATA_ID: [data_id],
             ACL_DATA_PATH: [data_path],
-            ACL_DTYPE: [str(data_val.dtype)],
-            ACL_SHAPE: [str(data_val.shape)],
             CMP_FLAG: [False]
         })
         csv_data = pd.concat([csv_data, row_data], ignore_index=True)
     else:
         index = mapping_data.index.values[0]
-        data_val.tofile(data_path)
+        np.save(data_path, data_val)
         csv_data[ACL_DATA_PATH][index] = data_path
-        csv_data[ACL_DTYPE][index] = str(data_val.dtype)
-        csv_data[ACL_SHAPE][index] = str(data_val.shape)
 
         # 对应的pta数据存在时，触发比对
         csv_data = compare_tensor(csv_data=csv_data)
@@ -204,151 +190,3 @@ def write_acl_map_file(tensor_path):
         with open(acl_map_file_path, mode="a") as file:
             file.write(tensor_path)
             file.write("\n")
-
-
-def compare_tensor(csv_data: pd.DataFrame):
-    csv_data.fillna(value="", inplace=True)
-    data = csv_data[csv_data[CMP_FLAG] == False]
-    if data.empty:
-        return csv_data
-
-    for idx in data.index:
-        pta_data_path, pta_dtype, pta_shape = _get_data_info(data, idx, data_src="pta")
-        acl_data_path, acl_dtype, acl_shape = _get_data_info(data, idx, data_src="acl")
-
-        if os.path.exists(pta_data_path):
-            pta_data = np.fromfile(pta_data_path, pta_dtype).reshape(pta_shape)
-        else:
-            csv_data[CMP_FAIL_REASON][idx] = "pta_data_path is not exist."
-            csv_data[CMP_FLAG][idx] = True
-            continue
-
-        if os.path.exists(acl_data_path):
-            if acl_dtype and acl_shape:
-                acl_data = np.fromfile(acl_data_path, acl_dtype).reshape(acl_shape)
-            else:
-                acl_data = read_acl_transformer_data(acl_data_path)
-        else:
-            csv_data[CMP_FAIL_REASON][idx] = "acl_data_path is not exist."
-            csv_data[CMP_FLAG][idx] = True
-            continue
-
-        pta_data_fp32 = pta_data.reshape(-1).astype("float32")
-        acl_data_fp32 = acl_data.reshape(-1).astype("float32")
-
-        csv_data[PTA_MAX_VALUE][idx] = np.max(pta_data_fp32)
-        csv_data[PTA_MIN_VALUE][idx] = np.min(pta_data_fp32)
-        csv_data[PTA_MEAN_VALUE][idx] = np.mean(pta_data_fp32)
-
-        csv_data[ACL_MAX_VALUE][idx] = np.max(acl_data_fp32)
-        csv_data[ACL_MIN_VALUE][idx] = np.min(acl_data_fp32)
-        csv_data[ACL_MEAN_VALUE][idx] = np.mean(acl_data_fp32)
-
-        for name, cmp_func in cmp_alg_map.items():
-            result = cmp_func(pta_data_fp32, acl_data_fp32)
-            csv_data[name][idx] = result
-            csv_data[CMP_FLAG][idx] = True
-
-    return csv_data
-
-
-def _get_data_info(data, idx, data_src):
-    if data_src == "pta":
-        path_key = PTA_DATA_PATH
-        dtype_key = PTA_DTYPE
-        shape_key = PTA_SHAPE
-    else:
-        path_key = ACL_DATA_PATH
-        dtype_key = ACL_DTYPE
-        shape_key = ACL_SHAPE
-
-    data_path = data[path_key][idx]
-    dtype = data[dtype_key][idx]
-    shape = data[shape_key][idx]
-    if isinstance(shape, str) and shape:
-        shape = [int(s) for s in shape[1:-1].split(',')]
-
-    if isinstance(dtype, str) and dtype:
-        dtype = np.dtype(dtype)
-
-    return data_path, dtype, shape
-
-
-class TensorBinFile:
-    def __init__(self, file_path) -> None:
-        self.file_path = file_path
-        self.dtype = 0
-        self.format = 0
-        self.dims = []
-
-        self.__parse_bin_file()
-
-    def get_tensor(self):
-        if self.dtype == 0:
-            dtype = np.float32
-        elif self.dtype == 1:
-            dtype = np.float16
-        elif self.dtype == 2:  # int8
-            dtype = np.int8
-        elif self.dtype == 3:  # int32
-            dtype = np.int32
-        elif self.dtype == 9:  # int64
-            dtype = np.int64
-        elif self.dtype == 12:
-            dtype = np.bool8
-        else:
-            logger.error("Unsupport dtype:", self.dtype)
-            pass
-        tensor = torch.tensor(np.frombuffer(self.obj_buffer, dtype=dtype))
-        tensor = tensor.view(self.dims)
-        return tensor
-
-    def __parse_bin_file(self):
-        with open(self.file_path, "rb") as fd:
-            file_data = fd.read()
-
-            begin_offset = 0
-            for i in range(len(file_data)):
-                if file_data[i] == ord("\n"):
-                    line = file_data[begin_offset: i].decode("utf-8")
-                    begin_offset = i + 1
-                    fields = line.split("=")
-                    attr_name = fields[0]
-                    attr_value = fields[1]
-                    if attr_name == ATTR_END:
-                        self.obj_buffer = file_data[i + 1:]
-                        break
-                    elif attr_name.startswith("$"):
-                        self.__parse_system_atrr(attr_name, attr_value)
-                    else:
-                        self.__parse_user_attr(attr_name, attr_value)
-                        pass
-
-    def __parse_system_atrr(self, attr_name, attr_value):
-        if attr_name == ATTR_OBJECT_LENGTH:
-            self.obj_len = int(attr_value)
-        elif attr_name == ATTR_OBJECT_PREFIX:
-
-            pass
-
-    def __parse_user_attr(self, attr_name, attr_value):
-        if attr_name == "dtype":
-            self.dtype = int(attr_value)
-        elif attr_name == "format":
-            self.format = int(attr_value)
-        elif attr_name == "dims":
-            self.dims = attr_value.split(",")
-            for i in range(len(self.dims)):
-                self.dims[i] = int(self.dims[i])
-
-
-def read_acl_transformer_data(file_path):
-    if not os.path.exists(file_path):
-        raise FileNotFoundError("{} is not exists".format(file_path))
-
-    if file_path.endswith(".bin"):
-        bin = TensorBinFile(file_path)
-        data = bin.get_tensor()
-        return data.cpu().numpy()
-
-    raise ValueError("Tensor file path must be end with .bin.")
