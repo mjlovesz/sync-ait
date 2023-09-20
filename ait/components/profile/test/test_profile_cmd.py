a50bce246924b935b6fa22d11dc55b19fe9a7461
yanhe13 <yanhe13@huawei.com>
2023-09-20 11:01:42 +0800

    bugfix v9

--- ait/components/profile/test/test_profile_cmd.py
+++ ait/components/profile/test/test_profile_cmd.py
@@ -30,7 +30,7 @@ class TestClass:
 
     def init(self):
         self.model_path = os.path.join(self.get_cur_path(),
-            "/../../benchmark/test/testdata/resnet50/model/pth_resnet50_bs1.om")
+            "../../benchmark/test/testdata/resnet50/model/pth_resnet50_bs1.om")
         self.output_path = os.path.join(self.get_cur_path(), "output_datas/")
         self.app_cmd = "'ait benchmark -om {}'".format(self.model_path)
 
