04bfd1bcc758f2dd8bc049cb9ca5e3b55df47b8d
yanhe13 <yanhe13@huawei.com>
2023-09-21 15:51:50 +0800

    ut fixedv27

--- ait/components/transplt/test/test_app_analyze.py
+++ ait/components/transplt/test/test_app_analyze.py
@@ -31,7 +31,8 @@ def transplt_argparse(argv):
     aa = get_cmd_instance()
     parser = argparse.ArgumentParser()
     aa.add_arguments(parser)
-    return parser.parse_args(argv)
+    args_parser = parser.parse_args(argv)
+    return args_parser
 
 
 def call_transplt_cmd(argv):
@@ -109,5 +110,5 @@ def test_app_analyze_given_opencv_csv_when_any_then_pass():
 
 def test_app_analyze_given_invalid_source_when_any_then_error():
     argv = ["-s", INVALID_ARG, "-f", REPORT_TYPE, "--log-level", LOG_LEVEL, "--tools", TOOLS]
-    with pytest.raises(argparse.ArgumentTypeError):
+    with pytest.raises(Exception):
         call_transplt_cmd(argv)
