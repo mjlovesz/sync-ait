02ce647020ca95f8862a0cef490b3d147277b4b1
yanhe13 <yanhe13@huawei.com>
2023-09-26 11:36:00 +0800

    textdecode bugfixedv6

--- onnx-modifier/static/om.js
+++ onnx-modifier/static/om.js
@@ -421,7 +421,7 @@ om.Graph = class {
                     let decodeKey = new TextDecoder("utf-8").decode(this._strList.keyList.list.s[keyIndex]);
                     let value = op.attr[key];
                     if (this._strList.mask.list.b[keyIndex]) {
-                        value.s = this._strList.valueList.list.s[value.list.i];
+                        value.s = this._strList.valueList.list.s[value.i];
                     }
                     op.attr[decodeKey] = value;
                     delete op.attr[key];
