33dd7fb716e92c455eb70f2e6e61c780c19fd790
yanhe13 <yanhe13@huawei.com>
2023-10-10 16:15:29 +0800

    find no CANN error

--- ait/components/benchmark/backend/setup.py
+++ ait/components/benchmark/backend/setup.py
@@ -92,6 +92,8 @@ def get_cann_path():
     set_env_path = os.getenv("CANN_PATH", "")
     if not set_env_path:
         set_env_path = os.environ.get("ASCEND_TOOLKIT_HOME")
+    if not set_env_path:
+        raise FileNotFoundError("can't find CANN path, please check.")
     set_env_path = set_env_path.split(':')[0]
     if not is_legal_args_path_string(set_env_path):
         raise TypeError(f"env CANN_PATH:{set_env_path} is illegal")
