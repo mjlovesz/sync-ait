5e43e843a2bb90f32bec2449e05edf7d47eb34de
yanhe13 <yanhe13@huawei.com>
2023-10-10 14:56:36 +0800

    cleancodev2

--- ait/components/benchmark/api_samples/interface_api_usage/multidevice_api/multidevice_infer_iteration_api.py
+++ ait/components/benchmark/api_samples/interface_api_usage/multidevice_api/multidevice_infer_iteration_api.py
@@ -18,16 +18,17 @@ from ais_bench.infer.interface import MultiDeviceSession
 def multidevice_infer_iteration_static():
     device_id = 0
     model_path = "../../sampledata/add_model/model/add_model_bs1.om"
-    multi_session = MultiDeviceSession(model_path)
     iteration_times = 1000
     in_out_list = [-1, 0]
-    # create new numpy data according inputs info
+    # create multidevice session of om model for inference
+    multi_session = MultiDeviceSession(model_path)
+    # create new numpy data
     shape1 = [1,3,32,32]
     shape2 = [1,3,32,32]
     ndata1 = np.full(shape1, 0).astype(np.float32)
     ndata2 = np.full(shape2, 0).astype(np.float32)
+    # create {device_id : input datas} dict
     device_feeds = {device_id:[[ndata1, ndata2],[ndata1, ndata2]]}
-
     # in is numpy list and output is numpy list
     outputs = multi_session.infer_iteration(device_feeds, in_out_list, iteration_times, mode='static')
 
