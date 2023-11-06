666f4219c679ec5ae490325b6d05f83dcf52db25
y30047306 <yinqian9@huawei.com>
2023-11-06 15:44:12 +0800

    cleancode

--- ait/components/debug/compare/msquickcmp/pta_acl_cmp/cmp_algorithm.py
+++ ait/components/debug/compare/msquickcmp/pta_acl_cmp/cmp_algorithm.py
@@ -31,6 +31,7 @@ def cosine_similarity(pta_data: np.ndarray, acl_data: np.ndarray):
     elif pta_data_norm ** 0.5 <= FLOAT_EPSILON:
         logger.warning('Cannot compare by Cosine Similarity. All the pta_data is zero')
         return NAN
+
     result = (acl_data / acl_data_norm) @ (pta_data / pta_data_norm)
     return '{:.6f}'.format(result)
 
@@ -40,7 +41,7 @@ def max_relative_error(pta_data: np.ndarray, acl_data: np.ndarray):
         np.abs(pta_data) > FLOAT_EPSILON,
         np.abs(acl_data / pta_data - 1),  # abs(aa - bb) / abs(bb) -> abs(aa / bb - 1)
         0,
-        ).max()
+    ).max()
     return result
 
 
