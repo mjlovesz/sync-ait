c439401b5631b39a2ce9cd359bf7a0cb9c5da85c
TAJh <taojiaheng1@huawei.com>
2023-11-21 03:51:08 +0000

    bugfix
    Signed-off-by: TAJh <taojiaheng1@huawei.com>

--- ait/components/debug/compare/msquickcmp/pta_acl_cmp/compare.py
+++ ait/components/debug/compare/msquickcmp/pta_acl_cmp/compare.py
@@ -74,8 +74,7 @@ def save_pta_data(csv_data, data_id, data_val, data_path):
 
         # 对应的acl_data存在时，触发比对
         dump_clean = os.getenv("AIT_DUMP_CLEAN")
-        if not dump_clean:
-            dump_clean = False
+        dump_clean = dump_clean == "True"
         csv_data = compare_tensor(csv_data=csv_data, dump_cleandump_clean)
 
     return csv_data
@@ -102,8 +101,7 @@ def save_acl_data(csv_data, data_id, data_val, data_path):
 
         # 对应的pta数据存在时，触发比对
         dump_clean = os.getenv("AIT_DUMP_CLEAN")
-        if not dump_clean:
-            dump_clean = False
+        dump_clean = dump_clean == "True"
         csv_data = compare_tensor(csv_data=csv_data, dump_clean=dump_clean)
 
     return csv_data
