dc7786dccb7ec80ac2c23f15490a6e8fb5bd0a37
王国伟 <wangguowei14@huawei.com>
2023-11-22 03:40:05 +0000

    update
    Signed-off-by: 王国伟 <wangguowei14@huawei.com>

--- ait/components/utils/file_open_check.py
+++ ait/components/utils/file_open_check.py
@@ -29,8 +29,6 @@ PATH_WHITE_LIST_REGEX = re.compile(r"[^_A-Za-z0-9/.-]")
 
 PERMISSION_NORMAL = 0o640  # 普通文件
 PERMISSION_KEY = 0o600  # 密钥文件
-READ_FILE_NOT_PERMITTED_STAT = stat.S_IWGRP | stat.S_IWOTH
-WRITE_FILE_NOT_PERMITTED_STAT = stat.S_IWGRP | stat.S_IWOTH | stat.S_IROTH | stat.S_IXOTH
 
 SOLUTION_LEVEL = 35
 SOLUTION_LEVEL_WIN = 45
@@ -182,22 +180,12 @@ class FileStat:
             solution_log(SOLUTION_BASE_URL + OWNER_SUB_URL)
             return False
         if perm == 'read':
-            if self.permission & READ_FILE_NOT_PERMITTED_STAT > 0:
-                logger.error(f"The file {self.file} is group writable, or is others writable, "
-                             "as import file(or directory) permission should not be over 0o755(rwxr-xr-x)")
-                solution_log(SOLUTION_BASE_URL + PERMISSION_SUB_URL)
-                return False
             if not os.access(self.realpath, os.R_OK) or self.permission & stat.S_IRUSR == 0:
                 logger.error(f"Current user doesn't have read permission to the file {self.file}, "
                              "as import file(or directory) permission should be at least 0o400(r--------) ")
                 solution_log(SOLUTION_BASE_URL + PERMISSION_SUB_URL)
                 return False
         elif perm == 'write' and self.is_exists:
-            if self.permission & WRITE_FILE_NOT_PERMITTED_STAT > 0:
-                logger.error(f"The file {self.file} is group writable, or is others writable, "
-                             "as export file(or directory) permission should not be over 0o750(rwxr-x---)")
-                solution_log(SOLUTION_BASE_URL + PERMISSION_SUB_URL)
-                return False
             if not os.access(self.realpath, os.W_OK):
                 logger.error(f"Current user doesn't have write permission to the file {self.file}, "
                              "as export file(or directory) permission should be at least 0o200(-w-------) ")
