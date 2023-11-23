c1cb38f8daa8179c1bce396b5977d8951db56656
18069865580 <jiangzhicheng6@huawei.com>
2023-11-23 19:55:24 +0800

    fix dt failure of transplt

--- ait/components/transplt/test/test.sh
+++ ait/components/transplt/test/test.sh
@@ -69,9 +69,6 @@ if [ $? != 0 ]; then
     exit 1
 fi
 
-PATH=/root/anaconda3/bin:$PATH
-export PATH=/root/anaconda3/bin:$PATH
-
 coverage combine $CUR_PATH
 coverage report -m --omit="test_*.py" > $CUR_PATH/test.coverage
 
