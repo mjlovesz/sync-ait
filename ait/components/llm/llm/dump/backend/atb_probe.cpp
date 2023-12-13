92d31042f9f910801b81163eade9f242e52c6cbd
TAJh <taojiaheng1@huawei.com>
2023-12-13 06:35:17 +0000

    bugfix
    Signed-off-by: TAJh <taojiaheng1@huawei.com>

--- ait/components/llm/llm/dump/backend/atb_probe.cpp
+++ ait/components/llm/llm/dump/backend/atb_probe.cpp
@@ -85,7 +85,10 @@ bool atb::Probe::IsTensorNeedSave(const std::vector<int64_t> &ids, const std::st
         for (auto &indice : splitVid) {
             bool result = false;
             if (IsSaveChild()) {
-                result = IsPrefix(query, indice);
+                result = IsPrefix(query, indice) &&
+                         (query == indice ||
+                         (query.length() > indice.length() &&
+                         query[indice.length()] == '_'));
             } else {
                 result = indice == query;
             }
@@ -105,6 +108,7 @@ bool atb::Probe::IsTensorNeedSave(const std::vector<int64_t> &ids, const std::st
         std::vector<std::string> splitTid = SplitString(tid, ',');
         for (auto &indice : splitTid) {
             if (IsPrefix(copyOptype, indice)) {
+                if 
                 return true;
             }
         }
