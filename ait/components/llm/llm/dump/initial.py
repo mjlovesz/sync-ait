04d9ffec8d5b40c05172e95fc78ab919a065be94
x30055916 <xuchuan18@huawei.com>
2024-01-29 18:25:07 +0800

    删除IOTensor冗余开关

--- ait/components/llm/llm/dump/initial.py
+++ ait/components/llm/llm/dump/initial.py
@@ -23,7 +23,7 @@ from llm.common.constant import ATB_HOME_PATH, ATB_SAVE_TENSOR_TIME, ATB_SAVE_TE
     ATB_SAVE_TENSOR_RUNNER, ATB_SAVE_TENSOR, ATB_SAVE_TENSOR_RANGE, \
     ATB_SAVE_TILING, LD_PRELOAD, ATB_OUTPUT_DIR, ATB_SAVE_CHILD, ATB_SAVE_TENSOR_PART, \
     ASCEND_TOOLKIT_HOME, ATB_PROB_LIB_WITH_ABI, ATB_PROB_LIB_WITHOUT_ABI, ATB_SAVE_CPU_PROFILING, \
-    ATB_SAVE_OPERATION_INFO, ATB_SAVE_KERNEL_INFO, ATB_CUR_PID
+    ATB_CUR_PID
 
 
 def is_use_cxx11():
@@ -71,8 +71,6 @@ def init_dump_task(args):
     os.environ[ATB_SAVE_TENSOR_PART] = str(args.save_tensor_part)
     os.environ[ATB_SAVE_CPU_PROFILING] = "1" if "cpu_profiling" in args.type else "0"
     os.environ[ATB_CUR_PID] = str(os.getpid())
-    os.environ[ATB_SAVE_OPERATION_INFO] = "1" if "op" in args.type else "0"
-    os.environ[ATB_SAVE_KERNEL_INFO] = "1" if "kernel" in args.type else "0"
 
     cann_path = os.environ.get(ASCEND_TOOLKIT_HOME, "/usr/local/Ascend/ascend-toolkit/latest")
     if not cann_path or not os.path.exists(cann_path):
