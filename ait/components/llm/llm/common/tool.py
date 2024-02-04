a512bf32fed01303900e37e19e113c65bcfd3a28
c00562468 <chenlian16@huawei.com>
2024-02-04 17:39:08 +0800

    fix

--- ait/components/llm/llm/common/tool.py
+++ ait/components/llm/llm/common/tool.py
@@ -11,10 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
+import array
 
 import torch
-import array
 
 from llm.common.constant import ATTR_END, ATTR_OBJECT_LENGTH
 from llm.common.log import logger
