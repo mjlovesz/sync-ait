51dc1c4f3c7616a798c02f44acb1524eb31a684e
王国伟 <wangguowei14@huawei.com>
2024-02-06 09:42:43 +0000

    update
    Signed-off-by: 王国伟 <wangguowei14@huawei.com>

--- ait/components/tests/test_ait.sh
+++ ait/components/tests/test_ait.sh
@@ -17,42 +17,36 @@
 declare -i ret_ok=0
 declare -i ret_failed=1
 CUR_PATH=$("pwd")
+ALL_VALID_TEST_CASES=(/analyze/ /benchmark/ /convert/ /debug/compare/ /debug/surgeon/ /llm/ /profile/ /transplt/)
+
+function is_path_in_all_valid_test_cases() {
+    for test_case in ${ALL_VALID_TEST_CASES[@]}; do
+        if [[ "$1" =~ "$test_case" ]]; then
+            echo 1
+            return
+        fi
+    done
+    echo 0
+}
 
 function get_modified_module_list() {
-    dt_list=(0 0 0 0 0 0 0 0)
     soft_link_path=/home/dcs-50/ait_test/ait/ait/components
     [[ -d $soft_link_path ]] || { echo "can't find origin dt data";return $ret_failed; }
     cur_testdata_path=$CUR_PATH/../benchmark/test/testdata
     [[ -d $cur_testdata_path ]] || { `ln -s $soft_link_path/benchmark/test/testdata $cur_testdata_path`; }
     modify_files=$CUR_PATH/../../../../modify_files.txt
+    RUN_TESTCASES=()
     if [[ -f $modify_files ]];then
         echo "found modify_files"
         while read line
         do
-            result=""
-            result=$(echo $line | grep "components/analyze")
-            [[ $result == "" ]] || { dt_list[0]=1;echo "run analyze DT"; }
-            result=""
-            result=$(echo $line | grep "components/benchmark")
-            [[ $result == "" ]] || { dt_list[1]=1;echo "run benchmark DT"; }
-            result=""
-            result=$(echo $line | grep "components/convert")
-            [[ $result == "" ]] || { dt_list[2]=1;echo "run convert DT"; }
-            result=""
-            result=$(echo $line | grep "components/debug/compare")
-            [[ $result == "" ]] || { dt_list[3]=1;echo "run compare DT"; }
-            result=""
-            result=$(echo $line | grep "components/debug/surgeon")
-            [[ $result == "" ]] || { dt_list[4]=1;echo "run surgeon DT"; }
-            result=""
-            result=$(echo $line | grep "components/profile")
-            [[ $result == "" ]] || { dt_list[5]=1;echo "run profile DT"; }
-            result=""
-            result=$(echo $line | grep "components/transplt")
-            [[ $result == "" ]] || { dt_list[6]=1;echo "run transplt DT"; }
-            result=""
-            result=$(echo $line | grep "components/llm")
-            [[ $result == "" ]] || { dt_list[7]=1;echo "run llm DT"; }
+            echo "line: $line"
+            for test_case in ${ALL_VALID_TEST_CASES[@]}; do
+                if [[ "$line" =~ "$test_case" ]]; then
+                    RUN_TESTCASES=(${RUN_TESTCASES[@]} "$test_case")
+                    echo "run $test_case DT"
+                fi
+            done
         done < $modify_files
     fi
 }
@@ -60,31 +54,36 @@ function get_modified_module_list() {
 main() {
     export dt_mode=${1:-"normal"} # or "pr"
     if [[ $dt_mode == "pr" ]];then
-        dt_list=get_modified_module_list
+        RUN_TESTCASES=get_modified_module_list
     else
-        dt_list=(1 1 1 1 1 1 1)
+        RUN_TESTCASES=$ALL_VALID_TEST_CASES
     fi
-    echo "dt_list ${dt_list[@]}"
+    echo "RUN_TESTCASES ${RUN_TESTCASES[@]}"
 
     failed_case_names=""
     all_part_test_ok=0
     TEST_CASES=( $(find ./* -name test.sh) )
     for test_case in ${TEST_CASES[@]}; do
-        echo ">>>> Current test_case=$test_case"
-        CASE_PATH=`dirname $test_case`
+        is_valid=is_path_in_all_valid_test_cases $test_case
+        echo ">>>> Current test_case=$test_case, is_valid=$is_valid"
 
+        if [ $is_valid -eq 0 ]; then
+            continue
+        fi
+
+        CASE_PATH=`dirname $test_case`
         cd $CASE_PATH
         bash test.sh
         cur_result=$?
         echo ">>>> test_case=$test_case, cur_result=$cur_result"
-        if [ "$cur_result" -eq "0" ]; then
+        if [ "$cur_result" -ne "0" ]; then
             failed_case_names="$failed_case_names, $test_case"
             all_part_test_ok=$(( $all_part_test_ok + $cur_result ))
         fi
         cd $CUR_PATH
     done
 
-    echo "failed_case_names: ${failed_case_names:2:}"  # Exclude the first ", "
+    echo "failed_case_names: ${failed_case_names:2}"  # Exclude the first ", "
     return $all_part_test_ok
 }
 
