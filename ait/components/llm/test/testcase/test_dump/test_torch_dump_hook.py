21bfbc38e820d949daf2fa0e1b878339e5301add
chenlian <chenlian16@huawei.com>
2024-02-06 09:15:22 +0000

    update ait/components/llm/test/testcase/test_dump/test_torch_dump_hook.py.
    Signed-off-by: chenlian <chenlian16@huawei.com>

--- ait/components/llm/test/testcase/test_dump/test_torch_dump_hook.py
+++ ait/components/llm/test/testcase/test_dump/test_torch_dump_hook.py
@@ -29,7 +29,7 @@ def test_hook_when_tp_default_then_save_inputs():
     x = torch.randn(4, 4)
     model(x)
     for name in MODEL_NAME_LIST:
-        except_input_path = os.path.join("./ait_dump", str(os.getpid())+"_cpu", "0", name, "input_exec1_0.pth")
+        except_input_path = os.path.join("./ait_dump", str(os.getpid()) + "_cpu", "0", name, "input_exec1_0.pth")
         except_output_path = os.path.join("./ait_dump", str(os.getpid()) + "_cpu", "0", name, "output_exec1.pth")
         assert os.path.exists(except_input_path)
         assert os.path.exists(except_output_path)
