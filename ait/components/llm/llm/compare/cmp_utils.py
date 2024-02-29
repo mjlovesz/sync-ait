5e73afb7bc11bae9866af1014973f181e79851d3
chenlian <chenlian16@huawei.com>
2024-02-29 13:48:42 +0000

    update ait/components/llm/llm/compare/cmp_utils.py.
    Signed-off-by: chenlian <chenlian16@huawei.com>

--- ait/components/llm/llm/compare/cmp_utils.py
+++ ait/components/llm/llm/compare/cmp_utils.py
@@ -7,29 +7,50 @@ MIN_LAYER_NUMBER = 10
 
 
 def search_layer_node(root_node: TreeNode):
-    child_op_type = [child_node.node_type for child_node in root_node.children]
-    if len(child_op_type) > MIN_LAYER_NUMBER:
-        op_type_counts = Counter(child_op_type)
-        most_count = op_type_counts.most_common(1)[0][1]
-        if most_count > MIN_LAYER_NUMBER / 2:
-            most_op_type = op_type_counts.most_common(1)[0][0]
-            return most_op_type
-    else:
-        for child_node in root_node.children:
-            search_layer_node(child_node)
+    layer_node_type = ""
 
-
-def get_layer_node(root_node: TreeNode, layer_type: str, layer_nodes: List):
-    for child_node in root_node.children:
-        if child_node.node_type == layer_type:
-            layer_nodes.append(child_node)
+    def run(node):
+        nonlocal layer_node_type
+        if layer_node_type:
+            return
+        child_op_type = [child_node.node_type for child_node in node.children]
+        if len(child_op_type) > MIN_LAYER_NUMBER:
+            op_type_counts = Counter(child_op_type)
+            most_count = op_type_counts.most_common(1)[0][1]
+            if most_count > MIN_LAYER_NUMBER / 2:
+                most_op_type = op_type_counts.most_common(1)[0][0]
+                layer_node_type = most_op_type
         else:
-            get_layer_node(child_node, layer_type, layer_nodes)
+            for child_node in node.children:
+                run(child_node)
+
+    run(root_node)
+    return layer_node_type
 
 
-def get_leaf_nodes(root_node, nodes):
-    for child_node in root_node.children:
-        if child_node.children:
-            get_leaf_nodes(child_node)
-        else:
-            nodes.append(child_node)
+def get_layer_node(root_node: TreeNode, layer_type: str):
+    all_layer_nodes = []
+
+    def run(node, layer_type, layer_nodes):
+        for child_node in node.children:
+            if child_node.node_type == layer_type:
+                layer_nodes.append(child_node)
+            else:
+                run(child_node, layer_type, layer_nodes)
+
+    run(root_node, layer_type, all_layer_nodes)
+    return all_layer_nodes
+
+
+def get_leaf_nodes(root_node):
+    all_leaf_nodes = []
+
+    def run(node, leaf_nodes):
+        for child_node in node.children:
+            if child_node.children:
+                run(child_node, leaf_nodes)
+            else:
+                leaf_nodes.append(child_node)
+
+    run(root_node, all_leaf_nodes)
+    return all_leaf_nodes
