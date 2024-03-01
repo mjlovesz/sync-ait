4d871c5e03db6dbafd54562fcee978316686102d
xuchuan18 <xuchuan18@huawei.com>
2024-03-01 18:01:29 +0800

    clean code

--- ait/components/llm/llm/compare/acc_cmp.py
+++ ait/components/llm/llm/compare/acc_cmp.py
@@ -444,7 +444,7 @@ def pair_torch_atb_nodes(g_nodes, m_nodes, op_mapping, op_tensor_mapping=None):
                 my_tensor_path = os.path.join(atb_node.tensor_path, "after", "outtensor0.bin")
                 golden_tensor_path = os.path.join(torch_node.tensor_path, "output.pth")
                 get_row_data(golden_tensor_path, my_tensor_path)
-                
+
     return compared_result
                     
 
@@ -475,7 +475,7 @@ def cmp_torch_atb_model(golden_json, my_json, torch_tensor_path, atb_tensor_path
                                             ATB_TORCH_CUSTOMIZED_OP_MAPPING, ATB_TORCH_CUSTOMIZED_OP_TENSOR_MAPPING))
 
     data_frame = pd.DataFrame(compared_result, columns=CSV_GOLDEN_HEADER)
-    save_compare_dataframe_to_csv(data_frame, output_path)
+    return save_compare_dataframe_to_csv(data_frame, output_path)
 
 
 def load_mapping(mapping_file_path):
@@ -492,6 +492,7 @@ def load_mapping(mapping_file_path):
         logger.info(msg)
     else:
         logger.debug("Using built-in op_mapping")
+    return
 
 
 def cmp_torch_atb_model_init(torch_model_topo_file, golden_path, my_path, output_path, mapping_file_path):
@@ -514,3 +515,4 @@ def cmp_torch_atb_model_init(torch_model_topo_file, golden_path, my_path, output
     else:
         msg = f"Cannot find atb model file path: {atb_model_topo_file_path}"
         logger.error(msg)
+    return
