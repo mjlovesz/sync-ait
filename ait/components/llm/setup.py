1ce05d6be0aa91b0cc6f823201c488507b4da2ef
chenlian <chenlian16@huawei.com>
2024-03-25 03:16:21 +0000

    update ait/components/llm/setup.py.
    Signed-off-by: chenlian <chenlian16@huawei.com>

--- ait/components/llm/setup.py
+++ ait/components/llm/setup.py
@@ -26,9 +26,9 @@ for root, dirs, files in os.walk('components/llm/ait_llm/opcheck/test_framework/
     opchecker_lib_src.append((os.path.join("/", root), [os.path.join(root, f) for f in files]))
 
 setup(
-    name='ait-ait_llm',
+    name='ait-llm',
     version='1.0',
-    description='Debug tools for large language model(ait_llm)',
+    description='Debug tools for large language model(llm)',
     url='https://gitee.com/ascend/ait/ait/components/llm',
     packages=find_packages(),
     license='Apache-2.0',
