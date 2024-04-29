2c1ab135ecfbbbf4463e8b9980347e816938856e
xuchuan18 <xuchuan18@huawei.com>
2024-04-29 14:14:03 +0800

    修改rope

--- ait/components/llm/ait_llm/opcheck/check_case/rope.py
+++ ait/components/llm/ait_llm/opcheck/check_case/rope.py
@@ -91,7 +91,7 @@ class OpcheckUnpadRopeOperation(operation_test.OperationTest):
             hidden_sizeq = in_tensors[0].size()[1]
             head_size = in_tensors[2].size()[1]
             q_head_num = hidden_sizeq // head_size
-            hidden_sizek = in_tensors[0].size()[1]
+            hidden_sizek = in_tensors[1].size()[1]
             k_head_num = hidden_sizek // head_size
         rot_dim = in_tensors[2].size()[1]
 
