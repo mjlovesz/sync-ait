6c1272874c16545a4129871e7e1a3334f92ead59
空白 <wangzhiwei104@huawei.com>
2024-05-07 16:11:50 +0800

    feat: 调整logger格式

--- ait/components/tensor_view/ait_tensor_view/logger.py
+++ ait/components/tensor_view/ait_tensor_view/logger.py
@@ -2,12 +2,12 @@ import logging
 
 
 def get_logger():
-    _logger = logging.getLogger("tensor_view_logger")
+    _logger = logging.getLogger("tensor-view")
     _logger.propagate = False
     _logger.setLevel(logging.INFO)
     if not _logger.handlers:
         stream_handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(message)s')
+        formatter = logging.Formatter('[%(asctime)s][%(name)s] %(message)s')
         stream_handler.setFormatter(formatter)
         _logger.addHandler(stream_handler)
     return _logger
