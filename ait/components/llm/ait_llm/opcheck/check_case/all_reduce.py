0d4642d6f5f9ea3f42441cf2203351d586a851ee
xuchuan18 <xuchuan18@huawei.com>
2024-05-14 11:34:48 +0800

    修改all_reduce用例

--- ait/components/llm/ait_llm/opcheck/check_case/all_reduce.py
+++ ait/components/llm/ait_llm/opcheck/check_case/all_reduce.py
@@ -17,6 +17,7 @@ import torch
 import torch_npu
 
 from ait_llm.opcheck import operation_test
+from ait_llm.common.tool import read_atb_data
 from ait_llm.common.log import logger
 
 
@@ -51,23 +52,53 @@ class OpcheckAllReduceOperation(operation_test.OperationTest):
         result = in_tensors[0]
         for i in range(1, len(in_tensors)):
             result = torch.mul(result, in_tensors[i])
-        return [result] 
+        return [result]
+
+    def get_new_in_tensors(self):
+        def get_tensor_path(new_tensor_path, tensor_type):
+            _tensor_path = [x for x in os.listdir(new_tensor_path) if x.startswith(tensor_type)]
+            _tensor_path.sort(key=lambda x:int(x.split(tensor_type)[1].split('.')[0]))  
+            _tensor_path = [os.path.join(new_tensor_path, x) for x in _tensor_path]
+            return _tensor_path
+ 
+        rank = self.op_param.get("rank", None) 
+        rank_root = self.op_param.get("rankRoot", None)
+        rank_size = self.op_param.get("rankSize", None)
+        new_in_tensors = []
+        for i in range(rank_root, rank_size):
+            old_did_pid = "_".join([str(rank), str(self.pid)])
+            new_did_pid = "_".join([str(i), str(int(self.pid) - int(rank) + i)])
+            new_tensor_path = self.tensor_path.replace(old_did_pid, new_did_pid)
+            if new_tensor_path:
+                if os.path.isdir(new_tensor_path):
+                    _in_tensor_path = get_tensor_path(new_tensor_path, "intensor")
+                    for path in _in_tensor_path:
+                        _in_tensor = read_atb_data(path).npu()
+                        new_in_tensors.append(_in_tensor) 
+                else:
+                    raise RuntimeError(f"{new_tensor_path} not valid")
+            else:
+                raise RuntimeError(f"{new_tensor_path} not valid")
+        
+        return new_in_tensors
 
     def golden_calc(self, in_tensors):
         all_reduce_type = self.op_param.get('allReduceType', None)
         backend = self.op_param.get('backend', None)
+
+        new_in_tensors = self.get_new_in_tensors()
                     
         if all_reduce_type == "sum":
             if backend == "lccl":
-                golden = self.lccl_sum_cal(in_tensors)
+                golden = self.lccl_sum_cal(new_in_tensors)
             else:
-                golden = self.sum_cal(in_tensors)
+                golden = self.sum_cal(new_in_tensors)
         elif all_reduce_type == "max":
-            golden = self.max_cal(in_tensors)
+            golden = self.max_cal(new_in_tensors)
         elif all_reduce_type == "min":
-            golden = self.min_cal(in_tensors)
+            golden = self.min_cal(new_in_tensors)
         elif all_reduce_type == "prod":
-            golden = self.prod_cal(in_tensors)
+            golden = self.prod_cal(new_in_tensors)
 
         return golden
 
