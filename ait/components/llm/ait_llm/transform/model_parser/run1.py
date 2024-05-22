d271c92f9715cbd623edf43bd0919d7e13c58d73
空白 <wangzhiwei104@huawei.com>
2024-05-22 15:20:18 +0800

    fix: 删除无用的run文件

--- ait/components/llm/ait_llm/transform/model_parser/run1.py
+++
@@ -1,7 +0,0 @@
-from transformers.models.bloom.modeling_bloom import BloomConfig, BloomForCausalLM
-
-from parser import model_to_json
-
-model = BloomForCausalLM(BloomConfig())
-
-model_to_json(model, "bloom")
--- ait/components/llm/ait_llm/transform/model_parser/run5.py
+++
@@ -1,5 +0,0 @@
-from transformers.models.starcoder2.modeling_starcoder2 import Starcoder2Config, Starcoder2ForCausalLM
-
-from parser import model_to_json
-
-model_to_json(Starcoder2ForCausalLM(Starcoder2Config()), "starcoder2")
